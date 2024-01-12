fe9f681e3cf951c03da9b4301bd4784a74755aed
Timber Borner <timber@opensenselab.org>
2024-01-12 16:47:50 +0100

    Fix no provider errors

--- src/app/catalog/components/issuer-catalog/issuer-catalog.component.spec.ts
+++ src/app/catalog/components/issuer-catalog/issuer-catalog.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { COMMON_MOCKS_PROVIDERS_WITH_SUBS } from "../../../mocks/mocks.module.spec";
+import { COMMON_IMPORTS } from "../../../common/badgr-common.module";
+import { RouterTestingModule } from "@angular/router/testing";
+import { TranslateTestingModule } from "ngx-translate-testing";
 
 import { IssuerCatalogComponent } from './issuer-catalog.component';
 
@@ -8,7 +12,15 @@ describe('IssuerCatalogComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ IssuerCatalogComponent ]
+      declarations: [ IssuerCatalogComponent ],
+      imports: [
+          ...COMMON_IMPORTS,
+          RouterTestingModule,
+          TranslateTestingModule.withTranslations('de', {}),
+      ],
+      providers: [
+          ...COMMON_MOCKS_PROVIDERS_WITH_SUBS,
+      ]
     })
     .compileComponents();
   }));
