3e1c59903a8cca6f66e35a83b0e36a5a2f65a744
jona159 <raabejonas@yahoo.de>
2024-02-29 12:30:25 +0100

    fetch from new endpoint

--- src/app/catalog/components/collectionbadge-catalog/collectionbadge-catalog.component.ts
+++ src/app/catalog/components/collectionbadge-catalog/collectionbadge-catalog.component.ts
@@ -11,6 +11,8 @@ import { CollectionBadge } from '../../../issuer/models/collectionbadge.model';
 import { CollectionBadgeManager } from '../../../issuer/services/collectionbadge-manager.service';
 import * as d3 from 'd3'
 import {OrgChart} from 'd3-org-chart'
+import { TOKEN_STORAGE_KEY } from '../../../../../src/app/common/services/session.service';
+
 
 @Component({
 	selector: 'app-collectionbadge-catalog',
@@ -26,7 +28,11 @@ export class CollectionBadgeCatalogComponent extends BaseRoutableComponent imple
 		return this.route.snapshot.params['id'];
 	}
 
+	
+
 	data: any = null
+
+	authToken: string = null
 
 
 	constructor(
@@ -50,41 +56,34 @@ export class CollectionBadgeCatalogComponent extends BaseRoutableComponent imple
 				)
 		);
 
+		try {
+			if (localStorage.getItem(TOKEN_STORAGE_KEY) || sessionStorage.getItem(TOKEN_STORAGE_KEY)) {
+				this.authToken = localStorage.getItem(TOKEN_STORAGE_KEY);
+			}
+		} catch (e) {}
+
 	}    
 
 	ngOnInit() {
-		// super.ngOnInit();
-			//   const radiusScale = d3.scaleSqrt().domain([min, max]).range([10, 100]);
-			//   data.forEach((d) => {
-			// 	d._radius = Math.round(radiusScale(d.value) * 10) / 10;
-			//   });
-			fetch('http://localhost:8000/public/all-collectionbadges')
+			fetch('http://localhost:8000/v2/collectionbadges/KHss2J1RSECTDWE3q30NSw', {
+				headers: {
+					'Authorization': `Bearer ${this.authToken}`,
+					'Content-Type': 'application/json' 
+				  },
+			})
+			 
 			  .then((d) => d.json())
 			  .then((data) => {
-				const [min, max] = [264, 956129]
 
-			//   })
-
-
-			// fetch(
-			// 	'https://raw.githubusercontent.com/bumbeishvili/sample-data/main/flatData.json'
-			//   )
-			// 	.then((d) => d.json())
-			// 	.then((data) => {
-			// 	  const [min, max] = d3.extent(data, (d) => d.value);
-			// 	  const radiusScale = d3.scaleSqrt().domain([min, max]).range([10, 100]);
-			// 	  data.forEach((d) => {
-			// 		d._radius = Math.round(radiusScale(d.value) * 10) / 10;
-			// 	  });
 			const collectionBadgeData = 
 			       { 
-					name: this.collectionBadge.apiModel.name,
-					description: this.collectionBadge.apiModel.description,
-					image: this.collectionBadge.apiModel.image,
+					name: data.result[0].name,
+					description:  data.result[0].description,
+					image:  data.result[0].image,
 					id: 'collectionBadge'
 				   }
  
-			const badgeData = data.map(d => {return {
+			const badgeData = data.result[0].assertions.map(d => {return {
 				name: d.name,
 				description: d.description,
 				image: d.image,
@@ -93,21 +92,25 @@ export class CollectionBadgeCatalogComponent extends BaseRoutableComponent imple
 
 			}})
 
-			const combinedData = [collectionBadgeData, badgeData[0]]
+			const combinedData = [collectionBadgeData, ...badgeData]
 			const chart = new OrgChart().compact(false)
-								
-		      
-			  //@ts-ignore
-			  chart.layoutBindings().top.linkY = (n) => n.y - 24;
-			  
-			//   chart.zoomBehavior(d3.zoom().on("zoom", null));
+
+			function diagonal(s, d) {
+
+				const path = `M ${s.y} ${s.x}
+						C ${(s.y + d.y) / 2} ${s.x},
+						  ${(s.y + d.y) / 2} ${d.x},
+						  ${d.y} ${d.x}`
+			
+				return path
+			  }
 		
 			  chart
 			    //@ts-ignore
 				.nodeHeight((d) => 200)
 			    //@ts-ignore
 				.nodeWidth((d) => 200)
-				.childrenMargin((d) => 180)
+				.childrenMargin((d) => 280)
 				.setActiveNodeCentered(false)
 				.container('.chart-container')
 				.nodeUpdate(function (d) {
@@ -123,12 +126,13 @@ export class CollectionBadgeCatalogComponent extends BaseRoutableComponent imple
 				.linkUpdate(function (d) {
 				  d3.select(this)
 					.attr('stroke-width', '5px')
-					.style('stroke', 'lightgray');
+					.style('stroke', 'lightgray')
+					.attr('d', function(d){ return diagonal(d, d.parent) })
 				})
 				.nodeContent(
 				  (d) =>{
 				  if (d.depth === 0) { // Check if the node is at the top level
-					return `<svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
+					return `<svg width="400" height="400" xmlns="http://www.w3.org/2000/svg">
 					<polygon points="100,10 
 									150,30 
 									180,70 
@@ -138,24 +142,27 @@ export class CollectionBadgeCatalogComponent extends BaseRoutableComponent imple
 									50,170 
 									20,130 
 									20,70 
-									50,30" 
-							 style="fill:darkblue;stroke:purple;stroke-width:5;fill-rule:evenodd;" />
-							 <rect x="70" y="30" width="60" height="60" rx="30" ry="30" style="fill:white;stroke:none;" />
+									50,30"  
+             style="fill:darkblue;stroke:purple;stroke-width:5;fill-rule:evenodd;" />
+				             <rect x="70" y="30" width="60" height="60" rx="30" ry="30" style="fill:white;stroke:none;" />
 							 <image href="${combinedData[0].image}" x="80" y="40" width="40" height="40" style="border-radius:50%;" />
     
 							 <rect x="30" y="85" width="140" height="40" rx="20" ry="20" style="fill:white;stroke:none;" />
-							 <text x="100" y="105" text-anchor="middle" alignment-baseline="middle" style="fill:black; font-size:12px; text-transform: uppercase;">${combinedData[0].name}</text>
-				  </svg>
+							 <text x="100" y="105" text-anchor="middle" alignment-baseline="middle" style="fill:black; font-size:12px; text-transform: uppercase;">
+							 <tspan x="100" dy="0">Kleider-</tspan>
+							 <tspan x="100" dy="1.2em">tauschparty</tspan>
+							 </text>
+							 </svg>
+				
 				  `;
 				} else{
-					return `<div style='width:100%;height:100%;'>
-					<svg  width="120" height="120">
-					<polygon points="60,4 116,34 116,70 60,100 4,70 4,34" 
-							 style="fill:lightgray;stroke:gray;stroke-width:2;" />
-							 <rect x="30" y="30" width="60" height="60" rx="30" ry="30" style="fill:white;stroke:none;" />
-        <image href="https://bumbeishvili.github.io/avatars/avatars/portrait18.png" x="40" y="40" width="40" height="40" style="border-radius:50%;" />
-				  </svg>
-					</div>`
+					return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="139" height="160" viewbox="0 0 138.56406460551017 160" style="filter: drop-shadow(rgba(255, 255, 255, 0.5) 0px 0px 10px);"><path fill="#ffg" d="M69.28203230275508 0L138.56406460551017 40L138.56406460551017 120L69.28203230275508 160L0 120L0 40Z"></path>
+					<image xlink:href="${this.collectionBadge.apiModel.image}" x="34.28203230275508" y="0" width="70" height="120" />
+					<text x="69.28203230275508" y="110" text-anchor="middle" style="fill: white; font-size: 12px;">Your Text Here</text>
+</svg>
+					</svg>
+				  
+				  `;
 			}})
 				.data(combinedData)
 				.render();
