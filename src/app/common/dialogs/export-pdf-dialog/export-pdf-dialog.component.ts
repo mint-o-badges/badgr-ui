75dfe8b431692f4999f03efa245219376f223e11
jona159 <raabejonas@yahoo.de>
2024-04-01 13:02:53 +0200

    move convert image function outside of generatePdf function

--- src/app/common/dialogs/export-pdf-dialog/export-pdf-dialog.component.ts
+++ src/app/common/dialogs/export-pdf-dialog/export-pdf-dialog.component.ts
@@ -127,6 +127,15 @@ export class ExportPdfDialog extends BaseDialog {
 		return image;
 	}
 
+	async convertImageToDataURL(imageSrc) {
+		let blob = await fetch(imageSrc).then((res) => res.blob());
+		return await new Promise((resolve) => {
+			let reader = new FileReader();
+			reader.onload = () => resolve(reader.result);
+			reader.readAsDataURL(blob);
+		});
+	}
+
 	async generateSingleBadgePdf(badge: RecipientBadgeInstance, markdown: HTMLElement) {
 		this.pdfError = undefined;
 		const badgeClass: ApiRecipientBadgeClass = badge.badgeClass;
@@ -137,23 +146,13 @@ export class ExportPdfDialog extends BaseDialog {
 		this.doc = new jsPDF();
 
 		let yPos = 0;
-		let xMargin = 10;
 
 		const pageWidth = this.doc.internal.pageSize.getWidth();
 		const pageHeight = this.doc.internal.pageSize.getHeight();
 		let cutoff = pageWidth - 27;
 
-		async function convertImageToDataURL(imageSrc) {
-			let blob = await fetch(imageSrc).then((res) => res.blob());
-			return await new Promise((resolve) => {
-				let reader = new FileReader();
-				reader.onload = () => resolve(reader.result);
-				reader.readAsDataURL(blob);
-			});
-		}
-
-		let oeb_logo = await convertImageToDataURL('assets/logos/Logo-OEB-3.png');
-		const badge_image = await convertImageToDataURL(badgeClass.image);
+		let oeb_logo = await this.convertImageToDataURL('assets/logos/Logo-OEB-3.png');
+		const badge_image = await this.convertImageToDataURL(badgeClass.image);
 
 		var svgContentClock = `
 		<svg xmlns="http://www.w3.org/2000/svg" height="14" width="12.25" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#492e98" d="M176 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h16V98.4C92.3 113.8 16 200 16 304c0 114.9 93.1 208 208 208s208-93.1 208-208c0-41.8-12.3-80.7-33.5-113.2l24.1-24.1c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L355.7 143c-28.1-23-62.2-38.8-99.7-44.6V64h16c17.7 0 32-14.3 32-32s-14.3-32-32-32H224 176zm72 192V320c0 13.3-10.7 24-24 24s-24-10.7-24-24V192c0-13.3 10.7-24 24-24s24 10.7 24 24z"/></svg>`;
