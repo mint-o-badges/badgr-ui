70c79e45ad68867211170d4bc8e75c7d6d7e0f92
YouQam <qamazyousef@gmail.com>
2024-04-08 18:44:52 +0200

    some updates based on PR review

--- src/app/public/components/about/about.component.ts
+++ src/app/public/components/about/about.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
-  mailAdrees = "hallo@openbadges.education";
+  mailAddress = "hallo@openbadges.education";
   mailBody= "Interesse an Open Educational Badges";
   constructor() { }
 
