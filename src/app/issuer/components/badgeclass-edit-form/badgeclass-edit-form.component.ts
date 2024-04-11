2ad5282c01d5e500b48bfcbb38cde72cfb1ec57d
jona159 <raabejonas@yahoo.de>
2024-04-11 10:48:25 +0200

    readd study load field to badge creation and show conditionally in badge detail view

--- src/app/issuer/components/badgeclass-edit-form/badgeclass-edit-form.component.ts
+++ src/app/issuer/components/badgeclass-edit-form/badgeclass-edit-form.component.ts
@@ -148,7 +148,7 @@ export class BadgeClassEditFormComponent extends BaseAuthenticatedRoutableCompon
 		.addControl('badge_description', '', Validators.required)
 		.addControl('badge_criteria_url', '')
 		.addControl('badge_criteria_text', '')
-		.addControl('badge_study_load', 1, [Validators.required, this.positiveInteger, Validators.max(1000)])
+		.addControl('badge_study_load', 0, [Validators.required, this.positiveIntegerOrNull, Validators.max(10000)])
 		.addControl('badge_category', '', Validators.required)
 		.addControl('badge_level', 'a1', Validators.required)
 		.addControl('badge_based_on', {
@@ -845,6 +845,13 @@ export class BadgeClassEditFormComponent extends BaseAuthenticatedRoutableCompon
 		const val = parseInt(control.value, 10);
 		if (isNaN(val) || val < 1) {
 			return { expires_amount: 'Must be a positive integer' };
+		}
+	}
+
+	positiveIntegerOrNull(control: AbstractControl) {
+		const val = parseInt(control.value, 10);
+		if (isNaN(val) || val < 0) {
+			return { duration: 'Must be greater or equal to 0' };
 		}
 	}
 
