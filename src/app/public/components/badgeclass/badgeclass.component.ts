ae73e524ac2abf8578828689cd8426c85603286d
jona159 <raabejonas@yahoo.de>
2024-04-29 14:50:29 +0200

    remove console log

--- src/app/public/components/badgeclass/badgeclass.component.ts
+++ src/app/public/components/badgeclass/badgeclass.component.ts
@@ -39,8 +39,6 @@ export class PublicBadgeClassComponent {
 			const service: PublicApiService = injector.get(PublicApiService);
 			return service.getBadgeClass(paramValue);
 		});
-
-		console.log(this.badgeClass);
 	}
 
 	get badgeClass(): PublicApiBadgeClassWithIssuer {
