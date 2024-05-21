9a7a3869e2235a3f0e8c1af42526d75af3c8451f
jona159 <raabejonas@yahoo.de>
2024-05-21 18:06:04 +0200

    fix: set value for criteria_text in submit

--- src/app/issuer/components/badgeclass-edit-form/badgeclass-edit-form.component.ts
+++ src/app/issuer/components/badgeclass-edit-form/badgeclass-edit-form.component.ts
@@ -414,28 +414,6 @@ export class BadgeClassEditFormComponent extends BaseAuthenticatedRoutableCompon
 			}
 		});
 
-		this.badgeClassForm.rawControl.statusChanges.subscribe((e) => {
-			let criteriaText = "*Folgende Kriterien sind auf Basis deiner Eingaben als Metadaten im Badge hinterlegt*: \n\n"
-			let participationText = `Du hast erfolgreich an **${this.badgeClassForm.value.badge_name}** teilgenommen.  \n\n `
-			let competenciesTextCaption = "Dabei hast du folgende Kompetenzen gestärkt: \n\n"
-
-			let competenciesText = this.badgeClassForm.value.competencies.map((competency) => {
-				return `- ${competency.name} \n`
-			}).join('');
-
-			let aiCompetenciesText = this.badgeClassForm.controls.aiCompetencies.controls.map((aiCompetency, index) => {
-				if(aiCompetency.controls.selected.value){
-					return `- ${this.aiCompetenciesSuggestions[index].preferred_label} \n`
-				}
-			}).join('');
-
-			if(this.badgeCategory === 'competency'){
-				this.badgeClassForm.controls.badge_criteria_text.setValue(criteriaText + participationText + competenciesTextCaption + competenciesText + aiCompetenciesText );
-			}else{
-				this.badgeClassForm.controls.badge_criteria_text.setValue(criteriaText + participationText );	
-			}
-
-		})
 		this.fetchTags();
 	}
 
@@ -739,6 +717,26 @@ export class BadgeClassEditFormComponent extends BaseAuthenticatedRoutableCompon
 						this.badgeClassForm.controls.competencies.removeAt(i);
 					}
 				});
+			}
+
+			let criteriaText = "*Folgende Kriterien sind auf Basis deiner Eingaben als Metadaten im Badge hinterlegt*: \n\n"
+			let participationText = `Du hast erfolgreich an **${this.badgeClassForm.value.badge_name}** teilgenommen.  \n\n `
+			let competenciesTextCaption = "Dabei hast du folgende Kompetenzen gestärkt: \n\n"
+
+			let competenciesText = this.badgeClassForm.value.competencies.map((competency) => {
+				return `- ${competency.name} \n`
+			}).join('');
+
+			let aiCompetenciesText = this.badgeClassForm.controls.aiCompetencies.controls.map((aiCompetency, index) => {
+				if(aiCompetency.controls.selected.value){
+					return `- ${this.aiCompetenciesSuggestions[index].preferred_label} \n`
+				}
+			}).join('');
+
+			if(this.badgeCategory === 'competency'){
+				this.badgeClassForm.controls.badge_criteria_text.setValue(criteriaText + participationText + competenciesTextCaption + competenciesText + aiCompetenciesText );
+			}else{
+				this.badgeClassForm.controls.badge_criteria_text.setValue(criteriaText + participationText );	
 			}
 
 			if (this.badgeClassForm.controls.badge_customImage.value && this.badgeClassForm.valid) {
