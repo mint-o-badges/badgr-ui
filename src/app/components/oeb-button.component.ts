ac7b911533f279084aaf0cbda81ef41497ef53a1
umut <u.tas@reedu.de>
2024-06-03 14:55:02 +0200

    fix: disabled buttons, page width 1440px

--- src/app/components/oeb-button.component.ts
+++ src/app/components/oeb-button.component.ts
@@ -1,14 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 import { HlmButtonDirective } from './spartan/ui-button-helm/src';
 
 @Component({
   selector: 'oeb-button',
   standalone: true,
   imports: [HlmButtonDirective],
-  template: ` <button class="tw-w-full" hlmBtn [size]="size" [variant]="variant"><ng-content></ng-content></button> `,
+  template: ` <button class="tw-w-full" hlmBtn [disabled]="disabled" [size]="size" [variant]="variant"><ng-content></ng-content></button> `,
 })
 export class OebButtonComponent {
 
     @Input() variant: string = 'default'; 
     @Input() size: string = 'default';
+    @Input() disabled: boolean = false;
 }
