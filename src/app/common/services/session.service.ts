b576924511075bd9591f6ca857abc7eb18f707f7
Timber <timber@opensenselab.org>
2024-06-04 14:29:15 +0200

    Use iframe for silent token renewal
    This utilizes the prompt=none query parameter for the backend which is
    passed via mozilla-django-oidc to prevent illegal frame renderings. Now
    the only issue that remains to be solved is the storing of the cookies,
    which get interpreted as third party cookies...

--- src/app/common/services/session.service.ts
+++ src/app/common/services/session.service.ts
@@ -115,6 +115,8 @@ export class SessionService {
                         console.error("Access tokens livetime is less than a minute!");
                         renewIn = 10;
                     }
+                    // TODO: Remove
+                    renewIn = 10;
                     // The bind is necessary because otherwise the called method can't figure out
                     // if the user is logged in
                     setTimeout(this.initiateSilentOidcTokenRenewal.bind(this), renewIn * 1000);
@@ -125,14 +127,19 @@ export class SessionService {
     }
 
     initiateSilentOidcTokenRenewal() {
-        // TODO: Do this silently
         if (!this.isLoggedIn) {
             console.log("Skipping silent OIDC token renewal, since user isn't logged in anymore");
             return;
         }
         console.log("Silently renewing the OIDC token...");
-        const endpoint = this.baseUrl + '/oidc/authenticate';
-        window.location.href = endpoint;
+        const endpoint = this.baseUrl + '/oidc/authenticate?prompt=none';
+        const iframe = <HTMLIFrameElement>document.getElementById("SilentOidcTokenRenewalIframe")
+        if (!iframe) {
+            console.error("Couldn't find iframe to renew OIDC token. Resorting to use the whole document...");
+            window.location.href = endpoint;
+            return;
+        }
+        iframe.src = endpoint;
     }
 
 	initiateUnauthenticatedExternalAuth(provider: ExternalAuthProvider) {
