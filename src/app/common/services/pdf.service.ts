79e0b39ca734db46175eb5365d46992b8a9f04f7
jona159 <raabejonas@yahoo.de>
2024-06-11 13:11:09 +0200

    fix: sanitize url before bypassing security

--- src/app/common/services/pdf.service.ts
+++ src/app/common/services/pdf.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, SecurityContext } from '@angular/core';
 import { AppConfigService } from '../app-config.service';
 import { BaseHttpApiService } from './base-http-api.service';
 import { MessageService } from './message.service';
@@ -27,7 +27,8 @@ export class PdfService {
         return this.http.get(`${this.baseUrl}/v1/earner/badges/pdf/${slug}`, { headers: headers, responseType: 'blob' }).pipe(
           map((response: Blob) => {
             const url = URL.createObjectURL(response);
-            return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+            const safe_url = this.sanitizer.sanitize(SecurityContext.URL, url);
+            return this.sanitizer.bypassSecurityTrustResourceUrl(safe_url);
           })
         );
       }
