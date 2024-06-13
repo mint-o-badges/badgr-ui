3a3159dd3cf1da0d28c3b62e2523fcb25ce65c85
umut <u.tas@reedu.de>
2024-06-13 13:50:59 +0200

    feat: apply style to startpage, fix components

--- src/app/components/spartan/ui-typography-helm/src/lib/hlm-h3.directive.ts
+++ src/app/components/spartan/ui-typography-helm/src/lib/hlm-h3.directive.ts
@@ -2,7 +2,7 @@ import { Directive, computed, input } from '@angular/core';
 import { hlm } from '@spartan-ng/ui-core';
 import type { ClassValue } from 'clsx';
 
-export const hlmH3 = 'md:tw-text-[22px] md:tw-leading-[26.4px] tw-text-purple tw-text-[16px] tw-leading-[19.2px]';
+export const hlmH3 = 'md:tw-text-[22px] md:tw-leading-[26.4px] tw-text-[16px] tw-leading-[19.2px]';
 
 @Directive({
 	selector: '[hlmH3]',
