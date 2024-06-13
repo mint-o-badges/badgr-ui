aa40a4781c2c357c44275140e650baa978c5515e
YouQam <qamazyousef@gmail.com>
2024-06-13 07:17:48 +0200

    fix: exclude user with unverified-email from being logged out

--- src/app/common/services/session.service.ts
+++ src/app/common/services/session.service.ts
@@ -52,8 +52,8 @@ export class SessionService {
 		private configService: AppConfigService,
 		private messageService: MessageService,
 		private navService: NavigationService,
-	) //public oAuthManager: OAuthManager,
-	{
+	) {
+		//public oAuthManager: OAuthManager,
 		this.baseUrl = this.configService.apiConfig.baseUrl;
 		this.enabledExternalAuthProviders = configService.featuresConfig.externalAuthProviders || [];
 	}
@@ -189,21 +189,26 @@ export class SessionService {
 	 * Handles errors from the API that indicate session expiration, invalid token, and other similar problems.
 	 */
 	handleAuthenticationError() {
-		this.logout();
+		// currently, only authenticated users (with verified emails) can load list of issuers, this cause users with unverified emails who wants to resend verification-email to be logged out immediatly. Checking whther user is already logged in should resolve the issue for now.
+		if (!this.isLoggedIn) {
+			this.logout();
 
-		if (this.navService.currentRouteData.publiclyAccessible !== true) {
-			const params = new URLSearchParams(document.location.search.substring(1));
-			// catch redirect_uri
-			const redirectUri = params.get('redirect_uri');
-			if (redirectUri) localStorage.redirectUri = redirectUri;
-			// If we're not on a public page, send the user to the login page with an error
-			window.location.replace(
-				`/auth/login?authError=${encodeURIComponent('Your session has expired. Please log in to continue.')}`,
-			);
-		} else {
-			// If we _are_ on a public page, reload the page after clearing the session token, because that will clear any messy error states from
-			// api errors.
-			window.location.replace(window.location.toString());
+			if (this.navService.currentRouteData.publiclyAccessible !== true) {
+				const params = new URLSearchParams(document.location.search.substring(1));
+				// catch redirect_uri
+				const redirectUri = params.get('redirect_uri');
+				if (redirectUri) localStorage.redirectUri = redirectUri;
+				// If we're not on a public page, send the user to the login page with an error
+				window.location.replace(
+					`/auth/login?authError=${encodeURIComponent(
+						'Your session has expired. Please log in to continue.',
+					)}`,
+				);
+			} else {
+				// If we _are_ on a public page, reload the page after clearing the session token, because that will clear any messy error states from
+				// api errors.
+				window.location.replace(window.location.toString());
+			}
 		}
 	}
 
@@ -211,8 +216,6 @@ export class SessionService {
 	 * To resend verification email for unlogged user.
 	 */
 	resendVerificationEmail_unloggedUser(emailToVerify: string) {
-		return this.http
-			.put<unknown>(this.baseUrl + `/v1/user/resendemail`, { email: emailToVerify })
-			.toPromise();
+		return this.http.put<unknown>(this.baseUrl + `/v1/user/resendemail`, { email: emailToVerify }).toPromise();
 	}
 }
