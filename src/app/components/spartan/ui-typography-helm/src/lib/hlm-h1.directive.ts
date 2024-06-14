2523bf125d3370a588984ad6249c9c54ea15ba4b
umut <u.tas@reedu.de>
2024-06-14 09:23:51 +0200

    fix: headline colors

--- src/app/components/spartan/ui-typography-helm/src/lib/hlm-h1.directive.ts
+++ src/app/components/spartan/ui-typography-helm/src/lib/hlm-h1.directive.ts
@@ -2,7 +2,7 @@ import { Directive, computed, input } from '@angular/core';
 import { hlm } from '@spartan-ng/ui-core';
 import type { ClassValue } from 'clsx';
 
-export const hlmH1 = 'md:tw-text-[46px] md:tw-leading-[55.2px] tw-text-purple tw-text-[30px] tw-leading-[36px]';
+export const hlmH1 = 'md:tw-text-[46px] md:tw-leading-[55.2px] tw-text-[30px] tw-leading-[36px]';
 
 @Directive({
 	selector: '[hlmH1]',
