c613d079fa46431234cc6149716839ba6e83e9b8
umut <u.tas@reedu.de>
2024-07-12 07:57:55 +0200

    fix: remove unused code

--- src/app/components/oeb-checkbox.component.ts
+++ src/app/components/oeb-checkbox.component.ts
@@ -6,50 +6,46 @@ import type { ClassValue } from 'clsx';
 import { hlm } from '@spartan-ng/ui-core';
 
 @Component({
-    selector: 'oeb-checkbox',
-    standalone: true,
-    imports: [HlmPDirective, HlmCheckboxComponent],      
-    providers: [
-        {
-            provide: NG_VALUE_ACCESSOR,
-            useExisting: forwardRef(() => OebCheckboxComponent),
-            multi: true,
-        }
-    ],
-    template: `
-        <label class="tw-flex tw-items-center" hlmP>
-            <hlm-checkbox [name]="name" (changed)="onChange($event)" class="tw-mr-2" />
-            {{text}}
-        </label>`,
-    host: {
+	selector: 'oeb-checkbox',
+	standalone: true,
+	imports: [HlmPDirective, HlmCheckboxComponent],
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			useExisting: forwardRef(() => OebCheckboxComponent),
+			multi: true,
+		},
+	],
+	template: ` <label class="tw-flex tw-items-center" hlmP>
+		<hlm-checkbox [name]="name" (changed)="onChange($event)" class="tw-mr-2" />
+		{{ text }}
+	</label>`,
+	host: {
 		'[class]': '_computedClass()',
 	},
 })
-export class OebCheckboxComponent implements ControlValueAccessor  {
-    @Input() text: string;
-	  @Input() control: FormControl;
-	  @Input() name: string;
+export class OebCheckboxComponent implements ControlValueAccessor {
+	@Input() text: string;
+	@Input() control: FormControl;
+	@Input() name: string;
 
-    @Input() ngModel: boolean;
-    @Input() value: string;
+	@Input() ngModel: boolean;
+	@Input() value: string;
 
-    // Output prop name must be Input prop name + 'Change'
-    // Use in your component to write an updated value back out to the parent
-    @Output() ngModelChange = new EventEmitter<string>();
+	@Output() ngModelChange = new EventEmitter<string>();
 
-    onChange(event){
-        this.ngModelChange.emit(event)
-    }
+	onChange(event) {
+		this.ngModelChange.emit(event);
+	}
 
-    //Angular needs these for custom implementations of ngModel
-    writeValue(value: string): void {
-        this.value = value ? value : ''
-    }
-    registerOnChange(fn: any): void { }
-    registerOnTouched(fn: any): void { }
-    setDisabledState?(isDisabled: boolean): void {  }
+	//Angular needs these for custom implementations of ngModel
+	writeValue(value: string): void {
+		this.value = value ? value : '';
+	}
+	registerOnChange(fn: Function): void {}
+	registerOnTouched(fn: Function): void {}
+	setDisabledState?(isDisabled: boolean): void {}
 
-    public readonly userClass = input<ClassValue>('', { alias: 'class' });
-   	protected _computedClass = computed(() => hlm(this.userClass()));
+	public readonly userClass = input<ClassValue>('', { alias: 'class' });
+	protected _computedClass = computed(() => hlm(this.userClass()));
 }
-
