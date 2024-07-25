cfb06ec8971b6094b898b6a193b0de63f5be7d30
YouQam <qamazyousef@gmail.com>
2024-07-25 13:12:06 +0200

    style: add color scale - best practice

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,63 +8,59 @@ import {
 const { fontFamily } = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
-  safelist: ['overflow-hidden'],
-  prefix: 'tw-',
-  content: [
-    "./src/**/*.{html,ts}",
-    './components/**/*.{html,ts}',
-  ],
-  theme: {
-    fontFamily: { body: ["rubik","Open Sans", 'sans-serif']},
-    container: {
+	presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
+	safelist: ['overflow-hidden'],
+	prefix: 'tw-',
+	content: ['./src/**/*.{html,ts}', './components/**/*.{html,ts}'],
+	theme: {
+		fontFamily: { body: ['rubik', 'Open Sans', 'sans-serif'] },
+		container: {
 			center: true,
 			padding: '2rem',
 			screens: {
 				'2xl': '1400px',
 			},
-    },
-    extend: {
-      colors: {
-        purple: 'var(--color-purple)',
-        lightpurple: 'var(--color-lightpurple)',
-        brightpurple: 'var(--color-brightpurple)',
-        buttonhover: 'var(--color-buttonhover)',
-        red: {
-          DEFAULT: 'var(--color-red)',
-          300: 'var(--color-lightred)',
-        },
-        pink: 'var(--color-pink)',
-        green: 'var(--color-green)',
-        yellow: 'var(--color-yellow)',
-        white: 'var(--color-white)',
-        lightgreen: 'var(--color-lightgreen)',
-        oebblack: 'var(--color-black)',
-        link: 'var(--color-link)',
-        oebgrey: 'var(--color-lightgray)',
-        grey85: 'var(--color-grey85)',
-        grey40: 'var(--color-grey40)'
-
-      },
-      gridTemplateColumns: {
-        'badges': 'repeat(auto-fill, minmax(320px, 1fr))',
-      }
-    },
-  },
-  plugins: [
-    scopedPreflightStyles({
-      isolationStrategy: isolateForComponents(
-        // selector string or array of selectors - the less/shorter - the better
-        [
-          '.oeb',
-        ],
-        // every strategy provides the same options (optional) to fine tune the transformation
-        {
-          // ignore: ["html", ":host", "*"], // when used, these will not be affected by the transformation
-          // remove: [":before", ":after"], // this can remove mentioned rules completely
-        },
-      ),
-    }),
-    require('tailwindcss-animate')
-  ],
-}
+		},
+		extend: {
+			colors: {
+				purple: 'var(--color-purple)',
+				lightpurple: 'var(--color-lightpurple)',
+				brightpurple: 'var(--color-brightpurple)',
+				buttonhover: 'var(--color-buttonhover)',
+				red: {
+					DEFAULT: 'var(--color-red)',
+					300: 'var(--color-lightred)',
+				},
+				pink: 'var(--color-pink)',
+				green: 'var(--color-green)',
+				yellow: 'var(--color-yellow)',
+				white: 'var(--color-white)',
+				lightgreen: 'var(--color-lightgreen)',
+				oebblack: 'var(--color-black)',
+				link: 'var(--color-link)',
+				oebgrey: 'var(--color-lightgray)',
+				grey: {
+					40: '#666666',
+					85: '#d9d9d9',
+				},
+			},
+			gridTemplateColumns: {
+				badges: 'repeat(auto-fill, minmax(320px, 1fr))',
+			},
+		},
+	},
+	plugins: [
+		scopedPreflightStyles({
+			isolationStrategy: isolateForComponents(
+				// selector string or array of selectors - the less/shorter - the better
+				['.oeb'],
+				// every strategy provides the same options (optional) to fine tune the transformation
+				{
+					// ignore: ["html", ":host", "*"], // when used, these will not be affected by the transformation
+					// remove: [":before", ":after"], // this can remove mentioned rules completely
+				},
+			),
+		}),
+		require('tailwindcss-animate'),
+	],
+};
