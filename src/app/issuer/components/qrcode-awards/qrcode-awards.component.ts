2dee80b66ef3eb5db8c3d73fd952d255a4adaf8f
jona159 <raabejonas@yahoo.de>
2024-08-05 20:42:30 +0200

    feat: improve qrcode component

--- src/app/issuer/components/qrcode-awards/qrcode-awards.component.ts
+++ src/app/issuer/components/qrcode-awards/qrcode-awards.component.ts
@@ -1,18 +1,19 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { BrnAccordionContentComponent, BrnAccordionDirective } from '@spartan-ng/ui-accordion-brain';
 import { HlmAccordionModule } from '../../../../app/components/spartan/ui-accordion-helm/src';
 import { HlmIconModule, provideIcons } from '../../../../app/components/spartan/ui-icon-helm/src';
 import { TranslateModule } from "@ngx-translate/core";
 import { RouterModule } from "@angular/router";
-import { NgIf } from "@angular/common";
+import { NgFor, NgIf } from "@angular/common";
 import { OebSeparatorComponent } from "../../../components/oeb-separator.component";
 import { OebButtonComponent } from "../../../components/oeb-button.component";
+import { BadgeRequestApiService } from "../../services/badgerequest-api.service";
 
 @Component({
 	selector: 'qrcode-awards',
 	templateUrl: './qrcode-awards.component.html',
     standalone: true,
-    providers: [BrnAccordionDirective],
+    providers: [BrnAccordionDirective, BadgeRequestApiService],
     imports: [
 		HlmAccordionModule,
 		HlmIconModule,
@@ -20,15 +21,27 @@ import { OebButtonComponent } from "../../../components/oeb-button.component";
 		BrnAccordionContentComponent,
 		RouterModule,
 		NgIf,
+		NgFor,
 		OebSeparatorComponent,
 		OebButtonComponent
 	],
 })
 
-export class QrCodeAwardsComponent {
-	separatorStyle = "tw-block tw-my-2 tw-border-[var(--color-lightgray)] tw-border"
+export class QrCodeAwardsComponent implements OnInit {
+
+	constructor(private badgeRequestApiService: BadgeRequestApiService) {}
+	separatorStyle = "tw-block tw-my-2 tw-border-[var(--color-lightgray)]"
 
     @Input() awards: any[]
 	@Input() routerLink: string[]
+	@Input() badgeClassSlug: string
+
+	requestedBadges: any[] = []
+
+	ngOnInit(): void {
+		this.badgeRequestApiService.getBadgeRequests(this.badgeClassSlug).then((requestedBadges) => {
+			console.log(requestedBadges)
+		})
+	}
 
 }
