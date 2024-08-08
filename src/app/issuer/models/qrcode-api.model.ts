b414b8f1a0adbc16ad265fee4493e899d8d7ec1b
jona159 <raabejonas@yahoo.de>
2024-08-08 10:56:18 +0200

    feat: add slug to qr code api model

--- src/app/issuer/models/qrcode-api.model.ts
+++ src/app/issuer/models/qrcode-api.model.ts
@@ -1,6 +1,7 @@
 export interface ApiQRCode{
     title: string;
-    createdBy: string; 
+    createdBy: string;
+    slug?: string; 
     expires_at?: any;
     badgeclass_id: string;
     issuer_id: string;
