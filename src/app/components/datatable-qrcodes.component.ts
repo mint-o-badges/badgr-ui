9eba98e697afcca55e789985761d435df52762c1
jona159 <raabejonas@yahoo.de>
2024-08-08 18:52:50 +0200

    feat: improve qr data table

--- src/app/components/datatable-qrcodes.component.ts
+++ src/app/components/datatable-qrcodes.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { HlmIconModule } from './spartan/ui-icon-helm/src';
 import { RouterModule } from '@angular/router';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { HlmTableModule } from './spartan/ui-table-helm/src';
 import { BadgeClass } from '../issuer/models/badgeclass.model';
 import { OebButtonComponent } from './oeb-button.component';
+import { BadgeRequestApiService } from '../issuer/services/badgerequest-api.service';
 
 @Component({
 	selector: 'qrcodes-datatable',
@@ -18,6 +19,7 @@ import { OebButtonComponent } from './oeb-button.component';
 		TranslateModule,
 		RouterModule
         ],
+    providers: [BadgeRequestApiService],    
 	template: `
         <hlm-table class="tw-rounded-[20px] tw-overflow-hidden tw-w-full tw-max-w-[100%] tw-bg-lightgray tw-border-darkgray tw-border">
             <hlm-caption>{{caption}}</hlm-caption>
@@ -30,16 +32,26 @@ import { OebButtonComponent } from './oeb-button.component';
                 <hlm-th class="!tw-flex-1 !tw-text-oebblack"><p class="u-text">{{badge.email}}</p></hlm-th>
                 <hlm-th class="tw-w-28 md:tw-w-48 tw-justify-center !tw-text-oebblack"> {{badge.requestedOn}}</hlm-th>
                 <hlm-th class="tw-justify-center sm:tw-justify-end sm:tw-w-48 tw-w-full !tw-text-oebblack">
-                    <oeb-button class="tw-w-full" variant="secondary" size="xs" width="full_width" (click)="actionElement.emit(badge.badge)" [text]="actionElementText"></oeb-button>
+                    <oeb-button class="tw-w-full" variant="secondary" size="xs" width="full_width" [routerLink]="" (click)="actionElement.emit(badge.badge)" [text]="actionElementText"></oeb-button>
                 </hlm-th>
             </hlm-trow>
         </hlm-table>`,
 })
-export class QrCodeDatatableComponent {
+export class QrCodeDatatableComponent implements OnInit {
 	@Input() caption: string = "";
-    @Input() requestedBadges: any[] = [{email: 'test@examplemail.com', requestedOn: "2021-10-10"}];
+    @Input() qrCodeId: string = "";
+    @Input() routerLink: string[] = [];
+    @Input() requestedBadges: any;
     @Input() actionElementText: string = "Badge vergeben"
     @Output() actionElement = new EventEmitter();
     @Output() redirectToBadgeDetail = new EventEmitter();
-}
 
+    constructor(private badgeRequestApiService: BadgeRequestApiService) {
+    }
+
+    ngOnInit(): void {
+        this.badgeRequestApiService.getBadgeRequestsByQrCode(this.qrCodeId).then((requestedBadges: any) => {
+            this.requestedBadges = requestedBadges.body.requested_badges
+         })
+        }
+    }
