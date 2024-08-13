cfd452e12896a9ea298704ed99214343a9278702
YouQam <qamazyousef@gmail.com>
2024-08-13 15:56:06 +0200

    feat: make button-icon size same as button

--- src/app/components/oeb-button.component.ts
+++ src/app/components/oeb-button.component.ts
@@ -3,15 +3,15 @@ import { HlmButtonDirective } from './spartan/ui-button-helm/src';
 import { NgIf } from '@angular/common';
 import { MessageService } from '../common/services/message.service';
 import { HlmIconModule, provideIcons } from './spartan/ui-icon-helm/src';
-import { lucideUpload } from '@ng-icons/lucide';
+import { lucidePlus, lucideUpload } from '@ng-icons/lucide';
 
 @Component({
   selector: 'oeb-button',
   standalone: true,
   imports: [HlmButtonDirective, NgIf, HlmIconModule],
-	providers: [MessageService, provideIcons({ lucideUpload })],
+	providers: [MessageService, provideIcons({ lucideUpload, lucidePlus })],
   template: `<button [type]="type" class="tw-relative" hlmBtn [disabled]="disabled" [width]="width" [size]="size" [variant]="variant">
-  			        <hlm-icon *ngIf="icon" class="tw-mr-4" size="base" [name]="icon" />
+  			        <hlm-icon *ngIf="icon" class="tw-mr-4" [size]="size" [name]="icon" />
                 <img *ngIf="img" class="md:tw-h-[30px] tw-h-[20px] tw-pr-4" [src]="img"/>
                 <span [innerHTML]="showLoadingMessage && loadingMessage ? loadingMessage : text"></span>
               </button> `,
