9f9f4d6c961ab69bc1426f79ff7a5f741de98408
jona159 <raabejonas@yahoo.de>
2024-08-29 15:55:25 +0200

    feat: limit issuer description

--- src/app/issuer/components/issuer-create/issuer-create.component.ts
+++ src/app/issuer/components/issuer-create/issuer-create.component.ts
@@ -31,7 +31,7 @@ export class IssuerCreateComponent extends BaseAuthenticatedRoutableComponent im
 
 	issuerForm = typedFormGroup()
 		.addControl('issuer_name', '', [Validators.required, Validators.maxLength(1024)])
-		.addControl('issuer_description', '', [Validators.required, Validators.maxLength(1024)])
+		.addControl('issuer_description', '', [Validators.required, Validators.maxLength(700)])
 		.addControl('issuer_email', '', [
 			Validators.required,
 			/*Validators.maxLength(75),
@@ -52,6 +52,7 @@ export class IssuerCreateComponent extends BaseAuthenticatedRoutableComponent im
 	emailsLoaded: Promise<unknown>;
 	verified = false;
 
+	enterDescription: string; 
 
 	constructor(
 		loginService: SessionService,
@@ -92,6 +93,9 @@ export class IssuerCreateComponent extends BaseAuthenticatedRoutableComponent im
 
 	ngOnInit() {
 		super.ngOnInit();
+		this.translate.get('Issuer.enterDescription').subscribe((translatedText: string) => {
+            this.enterDescription = translatedText;
+		});
 	}
 
 	
