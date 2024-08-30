46732c985af6d0fe950c6f0cde792fcb24c6af6d
YouQam <qamazyousef@gmail.com>
2024-08-30 14:14:57 +0200

    style: adjust data-table style

--- src/app/components/datatable-badges.component.ts
+++ src/app/components/datatable-badges.component.ts
@@ -22,8 +22,8 @@ import { OebButtonComponent } from './oeb-button.component';
         <hlm-table class="tw-rounded-t-[20px] tw-overflow-hidden tw-w-full tw-max-w-[100%] tw-bg-lightpurple tw-border-purple tw-border">
             <hlm-caption>{{caption}}</hlm-caption>
             <hlm-trow class="tw-bg-purple tw-text-white tw-flex-wrap hover:tw-bg-purple">
-                <hlm-th class="!tw-text-white tw-w-28 md:tw-w-48">Badge</hlm-th>
-                <hlm-th class="!tw-text-white tw-justify-center !tw-flex-1">{{'Badge.createdOn' | translate}}</hlm-th>
+                <hlm-th class="!tw-text-white tw-w-28 sm:tw-w-20 md:tw-w-40">Badge</hlm-th>
+                <hlm-th class="!tw-text-white tw-justify-center !tw-flex-1 tw-w-28 md:tw-w-48">{{'Badge.createdOn' | translate}}</hlm-th>
                 <hlm-th class="!tw-text-white tw-w-36 md:tw-w-40 tw-hidden sm:tw-grid">{{'Badge.multiRecipients' | translate}}</hlm-th>
                 <hlm-th class="!tw-text-white tw-justify-end sm:tw-w-48 tw-w-0 !tw-p-0"></hlm-th>
             </hlm-trow>
@@ -41,10 +41,10 @@ import { OebButtonComponent } from './oeb-button.component';
                       </div>  
                     </div>    
                 </hlm-th>
-                <hlm-th class="!tw-flex-1 tw-justify-center !tw-text-oebblack"><p class="u-text">{{badge.badge.createdAt | date:"dd.MM.yyyy"}}</p></hlm-th>
+                <hlm-th class="!tw-flex-1 tw-justify-center !tw-text-oebblack tw-w-28 md:tw-w-48"><p class="u-text">{{badge.badge.createdAt | date:"dd.MM.yyyy"}}</p></hlm-th>
                 <hlm-th class="tw-w-36 md:tw-w-40 tw-justify-center !tw-text-oebblack tw-hidden sm:tw-grid">{{badge.badge.recipientCount}}</hlm-th>
                 <hlm-th
-				class="tw-justify-center sm:tw-justify-end !tw-text-oebblack tw-flex-col tw-h-fit sm:tw-w-max tw-w-full tw-gap-2 tw-my-2 sm:tw-my-0"
+				class="tw-justify-center sm:tw-justify-end !tw-text-oebblack tw-flex-col tw-h-fit sm:tw-w-max tw-w-full tw-gap-2 tw-my-2"
                 >
                     <oeb-button
                         variant="secondary"
