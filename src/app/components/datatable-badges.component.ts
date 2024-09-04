98e82a7283aa551dad8892ee0ee91587c8c76036
YouQam <qamazyousef@gmail.com>
2024-09-04 12:53:06 +0200

    fix: update long badge title view on mobile view

--- src/app/components/datatable-badges.component.ts
+++ src/app/components/datatable-badges.component.ts
@@ -24,9 +24,13 @@ import { HlmPDirective } from '../components/spartan/ui-typography-helm/src/lib/
         <hlm-table class="tw-rounded-t-[20px] tw-overflow-hidden tw-w-full tw-max-w-[100%] tw-bg-lightpurple tw-border-purple tw-border">
             <hlm-caption>{{caption}}</hlm-caption>
             <hlm-trow class="tw-bg-purple tw-text-white tw-flex-wrap hover:tw-bg-purple">
+                <!-- Badge -->
                 <hlm-th class="!tw-text-white tw-w-28 sm:tw-w-20 md:tw-w-40">Badge</hlm-th>
+                <!-- Created on -->
                 <hlm-th class="!tw-text-white tw-justify-center !tw-flex-1 tw-w-24 sm:tw-w-28  md:tw-w-48 !tw-px-3 sm:!tw-px-4">{{'Badge.createdOn' | translate}}</hlm-th>
-                <hlm-th class="!tw-text-white tw-w-36 md:tw-w-40 sm:tw-grid">{{'Badge.multiRecipients' | translate}}</hlm-th>
+                <!-- Recipients -->
+                <hlm-th class="!tw-text-white tw-w-36 md:tw-w-40 sm:tw-grid sm:tw-pl-0">{{'Badge.multiRecipients' | translate}}</hlm-th>
+                <!-- Badge/QR-code award -->
                 <hlm-th class="!tw-text-white tw-justify-end sm:tw-w-48 tw-w-0 !tw-p-0"></hlm-th>
             </hlm-trow>
             <hlm-trow *ngFor="let badge of badges" class="tw-border-purple tw-flex-wrap tw-py-2">
@@ -37,8 +41,8 @@ import { HlmPDirective } from '../components/spartan/ui-typography-helm/src/lib/
                         alt="{{ badge.badge.description }}"
                         width="40"
                     />
-                    <div class="md:tw-grid md:tw-grid-cols-[150px] ">
-                      <div class="tw-line-clamp-2 tw-break-all">
+                    <div class="md:tw-grid md:tw-grid-cols-[150px] tw-my-1 md:tw-my-0">
+                      <div class="tw-line-clamp-1 md:tw-line-clamp-2 tw-break-all  tw-max-w-36 md:tw-max-w-none tw-absolute md:tw-relative">
                         <span class="tw-text-oebblack tw-cursor-pointer" (click)="redirectToBadgeDetail.emit(badge.badge)">{{badge.badge.name}}</span>
                       </div>  
                     </div>    
@@ -46,7 +50,7 @@ import { HlmPDirective } from '../components/spartan/ui-typography-helm/src/lib/
                 <hlm-th class="!tw-flex-1 tw-justify-center !tw-text-oebblack tw-w-24 sm:tw-w-28 md:tw-w-48 sm:tw-px-3 tw-px-4"><p hlmP class="tw-font-normal sm:!tw-text-[16px]">{{badge.badge.createdAt | date:"dd.MM.yyyy"}}</p></hlm-th>
                 <hlm-th class="tw-w-36 md:tw-w-40 tw-justify-center !tw-text-oebblack sm:tw-grid"><p hlmP class="tw-font-normal sm:!tw-text-[16px]">{{badge.badge.recipientCount}}</p></hlm-th>
                 <hlm-th
-				class="tw-justify-center sm:tw-justify-end !tw-text-oebblack tw-flex-col tw-h-fit sm:tw-w-max tw-w-full tw-gap-2 tw-my-2 tw-mt-6 sm:tw-mt-0"
+				class="tw-justify-center sm:tw-justify-end !tw-text-oebblack tw-flex-col tw-h-fit sm:tw-w-max tw-w-full tw-gap-2 tw-my-2 tw-mt-7 sm:tw-mt-2"
                 >
                     <oeb-button
                         variant="secondary"
