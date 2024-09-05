a39014db8614ce046789f4e30515ca361d7e38f7
jona159 <raabejonas@yahoo.de>
2024-09-05 15:56:46 +0200

    feat: remove unused function

--- src/app/issuer/components/badgeclass-generate-qr/badgeclass-generate-qr.component.ts
+++ src/app/issuer/components/badgeclass-generate-qr/badgeclass-generate-qr.component.ts
@@ -185,18 +185,7 @@ export class BadgeClassGenerateQrComponent extends BaseAuthenticatedRoutableComp
 		if (parentElement) {
 		  let data = await this.getQrCodePdf(parentElement)
 		}
-	  }
-	
-	private convertBase64ToBlob(Base64Image: string) {
-		const parts = Base64Image.split(";base64,")
-		const imageType = parts[0].split(":")[1]
-		const decodedData = window.atob(parts[1])
-		const uInt8Array = new Uint8Array(decodedData.length)
-		for (let i = 0; i < decodedData.length; ++i) {
-			uInt8Array[i] = decodedData.charCodeAt(i)
-		}
-		return new Blob([uInt8Array], { type: imageType })
-		}	
+	  }	
 
 	public openDangerDialog() {
 		const dialogRef = this._hlmDialogService.open(DangerDialogComponent, {
