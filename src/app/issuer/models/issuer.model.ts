d9782ea331834c09b28072dd244a72e9346cbf14
YouQam <qamazyousef@gmail.com>
2024-09-12 14:09:34 +0200

    feat: disable staff role from creating badges

--- src/app/issuer/models/issuer.model.ts
+++ src/app/issuer/models/issuer.model.ts
@@ -188,10 +188,6 @@ export class IssuerStaffMember extends ManagedEntity<ApiIssuerStaff, IssuerStaff
 		return this.roleSlug === 'editor';
 	}
 
-	get isStaff(): boolean {
-		return this.roleSlug === 'staff';
-	}
-
     /**
      * Evaluates if the user has the permission to make edits,
      * specifically to create badges. This is the case if the user
@@ -200,7 +196,7 @@ export class IssuerStaffMember extends ManagedEntity<ApiIssuerStaff, IssuerStaff
      * @returns {boolean}
      */
     get canEdit(): boolean {
-        return this.isOwner || this.isEditor || this.isStaff;
+        return this.isOwner || this.isEditor;
     }
 
 	/**
