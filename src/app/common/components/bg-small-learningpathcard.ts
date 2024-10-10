720da6067c590b24910fa6fab676d34287da8896
umut <u.tas@reedu.de>
2024-10-10 09:13:30 +0200

    fix: lp cards small, backpack things

--- src/app/common/components/bg-small-learningpathcard.ts
+++ src/app/common/components/bg-small-learningpathcard.ts
@@ -4,30 +4,30 @@ import { LearningPathApiService } from '../services/learningpath-api.service';
 type MatchOrProgressType = { match?: number, progress?: number };
 
 @Component({
-	selector: 'bg-learningpathcard',
+	selector: 'bg-small-learningpathcard',
 	host: {
-		class: 'tw-rounded-[10px] tw-w-[255px] md:tw-w-[392px] tw-border-solid tw-relative tw-p-4 md:tw-p-6 tw-block tw-overflow-hidden oeb-badge-card',
+		class: 'tw-rounded-[10px] tw-w-[255px] tw-border-solid tw-relative tw-p-4 tw-block tw-overflow-hidden oeb-badge-card',
 	},
 	template: `
 		<a [routerLink]="['/public/learningpaths/', slug]">
 			<div class="tw-flex tw-flex-col tw-justify-between tw-h-full">
-				<div class="tw-bg-[var(--color-lightgray)] tw-w-full tw-relative md:tw-h-[175px] tw-h-[100px] tw-items-center tw-flex tw-justify-center tw-p-2 tw-rounded-[3px]">
+				<div class="tw-bg-[var(--color-lightgray)] tw-w-full tw-relative tw-h-[100px] tw-items-center tw-flex tw-justify-center tw-p-2 tw-rounded-[3px]">
 				<div *ngIf="!completed" class="tw-absolute tw-top-[10px] tw-right-[10px]">
-					<img src="/assets/oeb/images/learningPath/learningPathIcon.png" class="md:tw-w-[30px] tw-w-[20px]"
+					<img src="/assets/oeb/images/learningPath/learningPathIcon.png" class=" tw-w-[20px]"
 						alt="LearningPath" />
 				</div>
 				<div *ngIf="completed" class="tw-absolute tw-top-[10px] tw-right-[10px] tw-flex tw-justify-center tw-items-center tw-gap-2">
 					<div class="tw-inline-block">
-						<img src="/assets/oeb/images/learningPath/learningPathIcon.png" class="md:tw-w-[30px] tw-w-[20px]"
+						<img src="/assets/oeb/images/learningPath/learningPathIcon.png" class="tw-w-[20px]"
 							alt="LearningPath" />
 					</div>
 					<div class="tw-bg-white tw-inline-flex tw-rounded-full tw-justify-center tw-items-center tw-border-solid tw-border-green tw-border-[3px] ">
-						<hlm-icon class="tw-text-purple tw-box-border md:tw-w-[26px] tw-w-[16px] md:tw-h-[26px] tw-h-[16px]" name="lucideCheck" />
+						<hlm-icon class="tw-text-purple tw-box-border tw-w-[16px] tw-h-[16px]" name="lucideCheck" />
 					</div>
 				</div>
 				
 					<img
-						class="md:tw-w-[145px] md:tw-h-[145px] tw-w-[65px] tw-h-[65px]"
+						class="tw-w-[65px] tw-h-[65px]"
 						[loaded-src]="badgeImage"
 						[loading-src]="badgeLoadingImageUrl"
 						[error-src]="badgeFailedImageUrl"
@@ -36,10 +36,10 @@ type MatchOrProgressType = { match?: number, progress?: number };
 				</div>
 				<div class="tw-flex tw-flex-col tw-flex-wrap tw-py-2 tw-text-oebblack tw-mt-2 tw-gap-1">
 					<span
-						class="tw-font-semibold tw-text-xs md:tw-text-[22px] md:tw-leading-[26px]"
+						class="tw-font-semibold tw-text-xs "
 						
 						>{{ name }}</span>
-						<a class="md:tw-text-[18px] md:tw-leading-[23.4px] tw-text-[10px] tw-leading-[13px]">{{issuerTitle}}</a>
+						<a class="tw-text-[10px] tw-leading-[13px]">{{issuerTitle}}</a>
 					<div class="tw-grid tw-items-center">
 						<div *ngIf="!isProgress" class="oeb-standard-padding-bottom tw-gap-1 tw-flex tw-flex-wrap">
 							<div hlmP size="sm" class="oeb-tag"
@@ -53,13 +53,13 @@ type MatchOrProgressType = { match?: number, progress?: number };
 							</div>
 						</div>	
 						<ng-template #progressBar>
-							<div *ngIf="progress === 0 || progress" class="tw-mb-4 tw-w-full tw-mt-2 md:tw-mt-6 tw-flex tw-justify-center tw-items-center">
-								<oeb-progress class="tw-w-full tw-h-5 md:tw-h-7 tw-relative tw-inline-flex tw-overflow-hidden tw-rounded-3xl tw-bg-white tw-items-center" [value]="(progress/studyLoad*100).toFixed(0)" [template]="progressTemplate"></oeb-progress>
+							<div *ngIf="progress === 0 || progress" class="tw-mb-2 tw-w-full tw-mt-2 tw-flex tw-justify-center tw-items-center">
+								<oeb-progress class="tw-w-full tw-h-5 tw-relative tw-inline-flex tw-overflow-hidden tw-rounded-3xl tw-bg-white tw-items-center" [value]="(progress/studyLoad*100).toFixed(0)" [template]="progressTemplate"></oeb-progress>
 							</div>
 						</ng-template>
 						<ng-template #progressTemplate>
 							<div class="tw-absolute tw-w-full tw-text-left">
-								<span class="tw-ml-2 md:tw-text-sm tw-text-[8px] tw-text-purple">Lernpfad <span *ngIf="!completed">{{(progress/studyLoad*100).toFixed(0)}}%</span> abgeschlossen</span>
+								<span class="tw-ml-2 tw-text-[8px] tw-text-purple">Lernpfad <span *ngIf="!completed">{{(progress/studyLoad*100).toFixed(0)}}%</span> abgeschlossen</span>
 							</div>
 						</ng-template>	
 						<oeb-button *ngIf="isProgress && progress/studyLoad === 1 && !completed && !requested" (click)="requestLearningPath()" [text]="'Lernpfad abholen'" width="full_width">
@@ -67,7 +67,7 @@ type MatchOrProgressType = { match?: number, progress?: number };
 						</oeb-button>
 						<p hlmP *ngIf="requested">Badge erfolgreich angefragt!</p>
 					</div>
-					<div class="tw-flex tw-flex-row tw-gap-4 tw-text-[#6B7280] md:tw-text-sm tw-text-[8px] tw-mt-2 md:tw-mt-6 tw-items-end">
+					<div class="tw-flex tw-flex-row tw-gap-4 tw-text-[#6B7280] tw-text-[8px] tw-mt-2 tw-items-end">
 						<hlm-icon name="lucideClock" />
 						<span>{{studyLoad | hourPipe}} Stunden</span>
 					</div>
@@ -76,7 +76,7 @@ type MatchOrProgressType = { match?: number, progress?: number };
 		</a>
 	`,
 })
-export class BgLearningPathCard {
+export class BgSmallLearningPathCard {
 	readonly badgeLoadingImageUrl = '../../../breakdown/static/images/badge-loading.svg';
 	readonly badgeFailedImageUrl = '../../../breakdown/static/images/badge-failed.svg';
 	private _matchOrProgress: MatchOrProgressType;
