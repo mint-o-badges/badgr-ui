d1c7590a07851acbfc4f82ee62c25a670e88cf59
Timber <timber@opensenselab.org>
2024-10-22 20:42:42 +0200

    feat: more logging

--- git-version.js
+++ git-version.js
@@ -4,17 +4,25 @@ const { version } = require('./package.json');
 const { resolve, relative } = require('path');
 const { writeFileSync, existsSync, mkdirSync } = require('fs-extra');
 
+console.log("Starting the file!");
+
 const gitInfo = gitDescribeSync({
     dirtyMark: false,
     dirtySemver: false
 });
 
+console.log("2");
+
 gitInfo.version = version;
+console.log("3");
 
 if (!existsSync(__dirname + '/src/environments')) {
+console.log("4");
     mkdirSync(__dirname + '/src/environments');
 }
+console.log("5");
 const file = resolve(__dirname, 'src', 'environments', 'version.ts');
+console.log("6");
 writeFileSync(file,
     `// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
@@ -22,5 +30,6 @@ writeFileSync(file,
 export const VERSION = ${JSON.stringify(gitInfo, null, 4)};
 /* tslint:enable */
 `, { encoding: 'utf-8' });
+console.log("7");
 
 console.log(`Wrote version info ${gitInfo.raw} to ${relative(resolve(__dirname, '..'), file)}`);
