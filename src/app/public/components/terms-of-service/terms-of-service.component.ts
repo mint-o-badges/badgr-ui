a3f29c6fa2fe1b5be622ccb85f12e43c80d4a2c1
jona159 <raabejonas@yahoo.de>
2024-10-29 07:43:21 +0100

    feat: wip

--- src/app/public/components/terms-of-service/terms-of-service.component.ts
+++ src/app/public/components/terms-of-service/terms-of-service.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BaseRoutableComponent } from '../../../common/pages/base-routable.component';
+
 
 @Component({
 	selector: 'app-terms',
 	templateUrl: './terms-of-service.component.html',
 	styleUrls: ['./terms-of-service.component.css'],
 })
-export class TermsComponent implements OnInit {
-	constructor() {}
+export class TermsComponent extends BaseRoutableComponent implements OnInit {
+	constructor(
+		router: Router,
+		route: ActivatedRoute,
+	) {
+		super(router, route);
+	}
 
-	ngOnInit() {}
+	ngOnInit() {
+		console.log(this.router.isActive)
+	}
 }
