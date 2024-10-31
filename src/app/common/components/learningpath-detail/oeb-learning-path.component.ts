3677b69b100792f04f2cf2c4c626adb998979f86
jona159 <raabejonas@yahoo.de>
2024-10-31 08:57:54 +0100

    fix: update graduates table after revocation

--- src/app/common/components/learningpath-detail/oeb-learning-path.component.ts
+++ src/app/common/components/learningpath-detail/oeb-learning-path.component.ts
@@ -168,8 +168,9 @@ export class OebLearningPathDetailComponent extends BaseRoutableComponent implem
 		return this.dialogService.confirmDialog;
 	}
 
-	revokeLpParticipationBadge(participant: any) {
+	async revokeLpParticipationBadge(participant: any) {
 		const participationBadgeInstance: BadgeInstance = participant.participationBadgeAssertion;
+
 		this.confirmDialog
 			.openResolveRejectDialog({
 				dialogTitle: 'Warnung',
@@ -177,19 +178,60 @@ export class OebLearningPathDetailComponent extends BaseRoutableComponent implem
 				resolveButtonLabel: 'Zurücknehmen',
 				rejectButtonLabel: 'Abbrechen',
 			})
-			.then(
-				() => {
-					this.badgeInstanceApiservice.revokeBadgeInstance(this.issuer.slug, this.learningPath.participationBadge_id, participationBadgeInstance.slug, 'revoked').then(
-						(result) => {
-							this.learningPathApiService.deleteLearningPathParticipant(participant.entity_id);
-						}),
-					(error) => {
-						console.log(error)
-					};
-				},
-				() => void 0, // Cancel
-			);
-	}
+			.then(async () => {
+
+				try {
+				  const [revokeResult, deleteResult] = await Promise.all([
+					this.badgeInstanceApiservice.revokeBadgeInstance(
+					  this.issuer.slug,
+					  this.learningPath.participationBadge_id,
+					  participationBadgeInstance.slug,
+					  'revoked'
+					),
+					this.learningPathApiService.deleteLearningPathParticipant(
+					  participant.entity_id
+					)
+				  ]);
+			  
+				  const response = await this.learningPathApiService.getLearningPathParticipants(
+					this.learningPath.slug
+				  );
+				  this.participants = response.body;
+				} catch (error) {
+				  console.error(error);
+				  throw error;
+				}
+			})
+
+	  }
+
+	// revokeLpParticipationBadge(participant: any) {
+	// 	const participationBadgeInstance: BadgeInstance = participant.participationBadgeAssertion;
+	// 	this.confirmDialog
+	// 		.openResolveRejectDialog({
+	// 			dialogTitle: 'Warnung',
+	// 			dialogBody: `Bist du sicher, dass du <strong>${this.learningPath.name}</strong> von <strong>${participationBadgeInstance.recipientIdentifier}</strong> zurücknehmen möchtest?`,
+	// 			resolveButtonLabel: 'Zurücknehmen',
+	// 			rejectButtonLabel: 'Abbrechen',
+	// 		})
+	// 		.then(
+	// 			() => {
+	// 				this.badgeInstanceApiservice.revokeBadgeInstance(this.issuer.slug, this.learningPath.participationBadge_id, participationBadgeInstance.slug, 'revoked').then(
+	// 					(result) => {
+	// 						this.learningPathApiService.deleteLearningPathParticipant(participant.entity_id).then(() => {
+	// 							this.learningPathApiService.getLearningPathParticipants(this.learningPath.slug).then(
+	// 								(participants) => {
+	// 									this.participants = participants.body;
+	// 								})
+	// 							})
+	// 					}),
+	// 				(error) => {
+	// 					console.log(error)
+	// 				};
+	// 			},
+	// 			() => void 0, // Cancel
+	// 		)
+	// }
 
 	downloadCertificate(participant: any ) {
 		const instance = participant.participationBadgeAssertion;
