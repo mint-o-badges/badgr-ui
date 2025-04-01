4a87a8d206bb9e1c176866ddb003132ab873fb78
jona159 <raabejonas@yahoo.de>
2025-04-01 10:54:35 +0200

    feat: change staff member role

--- src/app/components/datatable-issuer-staff.component.ts
+++ src/app/components/datatable-issuer-staff.component.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { HlmIconModule } from './spartan/ui-icon-helm/src';
 import { RouterModule } from '@angular/router';
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, SimpleChanges } from '@angular/core';
 import { HlmTableModule } from './spartan/ui-table-helm/src';
 import { OebButtonComponent } from './oeb-button.component';
 import { HlmPDirective } from '../components/spartan/ui-typography-helm/src/lib/hlm-p.directive';
 import { FormControl } from '@angular/forms';
 import { OebSelectComponent } from './select.component';
 import { IssuerStaffMember } from '../issuer/models/issuer.model';
+import { IssuerStaffRoleSlug } from '../issuer/models/issuer-api.model';
 
 @Component({
 	selector: 'issuer-staff-datatable',
@@ -28,33 +29,37 @@ import { IssuerStaffMember } from '../issuer/models/issuer.model';
 	>
 		<hlm-trow class="!tw-bg-lightgrey tw-text-oebblack tw-flex-wrap hover:tw-bg-lightgrey">
 			<!-- Name -->
-			<hlm-th class="tw-text-oebblack tw-w-[25%] tw-px-4">Name</hlm-th>
+			<hlm-th class="tw-text-oebblack md:tw-w-[25%] tw-w-[33%] tw-px-4">Name</hlm-th>
 			<!-- E-Mail -->
-			<hlm-th class="tw-text-oebblack tw-w-[25%] tw-px-4">E-Mail</hlm-th>
+			<hlm-th class="tw-text-oebblack md:tw-w-[25%] tw-w-[33%] tw-px-4">E-Mail</hlm-th>
 			<!-- Role -->
-			<hlm-th class="tw-text-oebblack tw-w-[25%] tw-px-4">Rolle</hlm-th>
+			<hlm-th class="tw-text-oebblack md:tw-w-[25%] tw-w-[33%] tw-px-4">Rolle</hlm-th>
 			<!-- Actions -->
-			<hlm-th class="tw-text-oebblack tw-w-[25%] tw-px-4"></hlm-th>
+			<hlm-th class="tw-text-oebblack md:tw-w-[25%] tw-w-0 tw-px-4"></hlm-th>
 		</hlm-trow>
 		<hlm-trow *ngFor="let member of members" class="tw-border-lightgrey tw-flex-wrap tw-py-2">
-			<hlm-th class="tw-w-[25%] tw-px-4 tw-items-center ">
+			<hlm-th class="md:tw-w-[25%] tw-w-[33%] tw-px-4 tw-items-center ">
 				<span class="tw-font-normal tw-text-lg tw-text-oebblack tw-truncate">{{ member.nameLabel }}</span>
 			</hlm-th>
-			<hlm-th class="tw-w-[25%] tw-px-4 tw-text-center tw-flex tw-items-center"
-				><p hlmP class="tw-font-normal tw-text-lg tw-text-oebblack">{{ member.email }}</p></hlm-th
+			<hlm-th class="md:tw-w-[25%] tw-w-[33%] tw-px-4 tw-text-center tw-flex tw-items-center"
+				><p hlmP class="tw-font-normal tw-text-lg tw-text-oebblack tw-truncate">{{ member.email }}</p></hlm-th
 			>
-			<hlm-th class="tw-w-36 md:tw-w-40 tw-justify-center !tw-text-oebblack sm:tw-grid">
-				<oeb-select
-					actionBar="true"
-					[placeholder]="member.roleInfo.label"
-					class="oeb tw-w-[12rem]"
-					[options]="roleOptions"
-					[control]="roleControl"
-					[autofocus]="true"
-					noTopMargin="true"
-				></oeb-select>
+			<hlm-th class="tw-w-36 md:tw-w-48 !tw-text-oebblack sm:tw-grid">
+				<div class="forminput forminput-full">
+					<div class="forminput-x-inputs">
+						<select
+							class="!tw-border-purple !tw-border-solid !tw-text-oebblack tw-rounded-[10px] tw-text-lg"
+							[ngModel]="member.roleSlug"
+							(change)="changeRole(member, $event.target.value)"
+						>
+							<option *ngFor="let role of roleOptions" [value]="role.value">
+								{{ role.label }}
+							</option>
+						</select>
+					</div>
+				</div>
 			</hlm-th>
-			<hlm-th class="tw-w-[25%] tw-px-4 tw-text-center tw-flex tw-justify-end">
+			<hlm-th class="md:tw-w-[25%] tw-w-full tw-px-4 tw-text-center tw-flex md:tw-justify-end">
 				<span (click)="removeMember(member)" class="tw-text-link tw-underline tw-text-sm tw-cursor-pointer">{{
 					'General.remove' | translate
 				}}</span>
@@ -65,16 +70,19 @@ import { IssuerStaffMember } from '../issuer/models/issuer.model';
 export class IssuerStaffDatatableComponent {
 	@Input() caption: string = '';
 	@Input() members: IssuerStaffMember[];
-	@Input() roleOptions: any[];
+	@Input() roleOptions: string[];
 	@Output() removeStaffMember = new EventEmitter();
-
-	roleControl = new FormControl('');
-
-	ngOnInit() {
-		console.log(this.roleOptions);
-	}
+	@Output() roleChanged = new EventEmitter<{ member: IssuerStaffMember; roleSlug: IssuerStaffRoleSlug }>();
 
 	removeMember(member: IssuerStaffMember) {
 		this.removeStaffMember.emit(member);
+	}
+
+	changeRole(member: IssuerStaffMember, roleSlug: IssuerStaffRoleSlug) {
+		this.roleChanged.emit({ member, roleSlug });
+	}
+
+	ngOnInit() {
+		console.log(this.members);
 	}
 }
