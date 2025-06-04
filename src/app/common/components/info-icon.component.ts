1d4ba655fce6ab4da0eb54a33650ea684e680acf
Stefan Trautvetter <stefan.trautvetter@esirion.de>
2025-06-04 15:20:51 +0200

    fix(info-icon): clean up

--- src/app/common/components/info-icon.component.ts
+++ src/app/common/components/info-icon.component.ts
@@ -1,10 +1,10 @@
-import { Component, input, Input, OnInit } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { NgIcon } from '@ng-icons/core';
 
 @Component({
 	selector: 'info-icon',
 	template: `
-		<div class="oeb-infoicon tw-relative tw-flex tw-items-center" [class]="{'oeb-infoicon--init': init}">
+		<div class="oeb-infoicon tw-relative tw-flex tw-items-center">
 			<ng-icon hlm name="lucideInfo" size="24px" class="tw-text-purple"></ng-icon>
 			<div class="oeb-infoicon__description tw-text-white tw-bg-purple tw-py-2 tw-px-3 tw-rounded-[10px] tw-ml-1" [style]="{ 'width': width() + 'px' }">
 				<ng-content/>
@@ -18,8 +18,6 @@ import { NgIcon } from '@ng-icons/core';
 			left: 100%;
 			opacity: 0;
 			visibility: hidden;
-		}
-		.oeb-infoicon--init .oeb-infoicon__description {
 			transition: opacity 0.2s ease 0.2s, visibility 0.2s linear 0.2s;
 		}
 		.oeb-infoicon:hover .oeb-infoicon__description {
@@ -32,15 +30,8 @@ import { NgIcon } from '@ng-icons/core';
 		NgIcon
 	]
 })
-export class InfoIcon implements OnInit {
+export class InfoIcon {
 	width = input(200);
 
 	init = false;
-
-	ngOnInit(): void {
-		// prevent css transition flashing
-		setTimeout(() => {
-			this.init = true;
-		}, 1)
-	}
 }
