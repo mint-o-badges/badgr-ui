fe82044a454e21bc1c7bff72dd2f19be218c5589
David Scheidt <david.scheidt@opensenselab.org>
2025-07-04 13:46:33 +0200

    refactor: badge catalog to use signals

--- src/app/catalog/components/badge-catalog/badge-catalog.component.ts
+++ src/app/catalog/components/badge-catalog/badge-catalog.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, computed, OnInit, signal } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MessageService } from '../../../common/services/message.service';
-import { Issuer } from '../../../issuer/models/issuer.model';
 import { Title } from '@angular/platform-browser';
-import { preloadImageURL } from '../../../common/util/file-util';
 import { AppConfigService } from '../../../common/app-config.service';
 import { BaseRoutableComponent } from '../../../common/pages/base-routable.component';
 import { BadgeClass } from '../../../issuer/models/badgeclass.model';
 import { BadgeClassManager } from '../../../issuer/services/badgeclass-manager.service';
 import { StringMatchingUtil } from '../../../common/util/string-matching-util';
-import { BadgeClassCategory } from '../../../issuer/models/badgeclass-api.model';
 import { TranslateService, TranslatePipe } from '@ngx-translate/core';
 import { FormControl, FormsModule } from '@angular/forms';
 import { appearAnimation } from '../../../common/animations/animations';
@@ -25,9 +22,11 @@ import { NgIcon } from '@ng-icons/core';
 import { HlmIconDirective } from '../../../components/spartan/ui-icon-helm/src/lib/hlm-icon.directive';
 import { OebGlobalSortSelectComponent } from '../../../components/oeb-global-sort-select.component';
 import { OebSelectComponent } from '../../../components/select.component';
-import { BgBadgecard } from '../../../common/components/bg-badgecard';
 import { PaginationAdvancedComponent } from '../../../components/oeb-numbered-pagination';
 import { SortPipe } from '../../../common/pipes/sortPipe';
+import { BgBadgecard } from '../../../common/components/bg-badgecard';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { combineLatest, map, startWith, switchMap } from 'rxjs';
 
 @Component({
 	selector: 'app-badge-catalog',
@@ -48,120 +47,103 @@ import { SortPipe } from '../../../common/pipes/sortPipe';
 		OebGlobalSortSelectComponent,
 		OebSelectComponent,
 		NgFor,
-		BgBadgecard,
 		PaginationAdvancedComponent,
 		SortPipe,
 		TranslatePipe,
+		BgBadgecard,
 	],
 })
 export class BadgeCatalogComponent extends BaseRoutableComponent implements OnInit {
-	badges = signal<BadgeClass[] | null>(null);
-	badgeResults = computed<BadgeClass[]>(() => this.getBadgeResults(this.badges()));
-	badgeResultsByIssuer = computed<MatchingBadgeIssuer[]>(() => this.getBadgeResultsByIssuer());
-	badgeResultsByCategory = computed<MatchingBadgeIssuer[]>(() => this.getBadgeResultsByCategory());
-	filteredBadges = computed<BadgeClass[] | null>(() => this.filterBadges());
+	/** Holds all badges known to the component */
+	badges = signal<BadgeClass[]>([]);
+
+	/** The tag selected for filtering {@link badges} into {@link filteredBadges} */
+	selectedTag = signal<ITag | null>(null);
+
+	/** A search string that is used to filter {@link badges} into {@link filteredBadges} */
+	searchQuery = signal<string>('');
+
+	/** A sorting option to sort {@link badges} into {@link filteredBadges} */
+	sortOption = signal<string | null>(null);
+
+	/** The 1-indexed current page the component sits on */
+	currentPage = signal<number>(1);
+
+	/** The number of badges shown per page */
+	badgesPerPage = signal<number>(30);
+
+	/** Determines whether a legend that explains badge categories is shown */
 	showLegend = signal<boolean>(false);
 
+	/** A subset of {@link badges} filtered using the values of {@link searchQuery}, {@link sortOption} and {@link selectedTag} */
+	filteredBadges = computed<BadgeClass[]>(() =>
+		this.filterBadges(this.badges(), this.searchQuery(), this.sortOption()),
+	);
+
+	/** The total number of pages taking into account the number of badges per page */
+	totalPages = computed<number>(() => Math.max(1, Math.ceil(this.filteredBadges().length / this.badgesPerPage())));
+
+	/** The subset of {@link filteredBadges} for the currently displayed page */
+	badgeResults = computed<BadgeClass[]>(() =>
+		this.filteredBadges().slice(
+			(this.currentPage() - 1) * this.badgesPerPage(),
+			(this.currentPage() - 1) * this.badgesPerPage() + this.badgesPerPage(),
+		),
+	);
+
+	/** Unique issuers of all badges */
+	issuers = computed<string[]>(() =>
+		this.badges()
+			.filter((b) => b.issuerVerified)
+			.flatMap((b) => b.issuer)
+			.filter((value, index, array) => array.indexOf(value) === index),
+	);
+
+	/** Selectable options to filter with */
+	tagsOptions = computed<ITag[]>(() =>
+		this.badges()
+			.flatMap((b) => b.tags)
+			.filter((value, index, array) => array.indexOf(value) === index)
+			.sort()
+			.map((t) => ({
+				label: t,
+				value: t,
+			})),
+	);
+
+	/** A string used for displaying the amount of badges that is aware of the current language */
+	badgesPluralWord = toSignal(
+		combineLatest(
+			[toObservable(this.badges), this.translate.onLangChange.pipe(startWith(this.translate.currentLang))],
+			(badges, lang) => badges,
+		).pipe(
+			map((badges) => {
+				if (badges.length === 0) return 'Badge.noBadges';
+				if (badges.length === 1) return 'Badge.oneBadge';
+				return 'Badge.multiBadges';
+			}),
+			switchMap((key) => this.translate.get(key)),
+		),
+	);
+
+	/** A string used for displaying the amount of issuers that is aware of the current language */
+	issuersPluralWord = toSignal(
+		combineLatest(
+			[toObservable(this.issuers), this.translate.onLangChange.pipe(startWith(this.translate.currentLang))],
+			(issuers, lang) => issuers,
+		).pipe(
+			map((issuers) => {
+				if (issuers.length === 0) return 'Badge.noIssuers';
+				if (issuers.length === 1) return 'Badge.oneIssuer';
+				return 'Badge.multiIssuers';
+			}),
+			switchMap((key) => this.translate.get(key)),
+		),
+	);
+
 	badgesLoaded: Promise<unknown>;
-
-	tags: string[] = [];
-	issuers: string[] = [];
-	selectedTag: string = null;
-
 	sortControl = new FormControl('');
-
-	groupOptions = [
-		{ value: '---', label: 'Nicht gruppieren' },
-		{ value: 'issuer', label: 'Institution' },
-		{ value: 'category', label: 'Kategorie' },
-	];
-	groupControl = new FormControl();
-
-	tagsOptions = [];
 	tagsControl = new FormControl();
-
-	badgesPerPage = 30;
-	totalPages: number;
-	nextLink: string;
-	previousLink: string;
-
-	sortOption: string | null = null;
-
-	plural = {
-		issuer: {
-			'=0': this.translate.instant('Badge.noIssuers'),
-			'=1': this.translate.instant('Badge.oneIssuer'),
-			other: this.translate.instant('Badge.multiIssuers'),
-		},
-		badges: {
-			'=0': this.translate.instant('Badge.noBadges'),
-			'=1': this.translate.instant('Badge.oneBadge'),
-			other: this.translate.instant('Badge.multiBadges'),
-		},
-		recipient: {
-			'=0': this.translate.instant('Badge.noRecipients'),
-			'=1': this.translate.instant('Badge.oneRecipient'),
-			other: this.translate.instant('Badge.multiRecipients'),
-		},
-	};
-
-	private _searchQuery = '';
-	get searchQuery() {
-		return this._searchQuery;
-	}
-	set searchQuery(query) {
-		this._searchQuery = query;
-		// this.updateResults();
-		this.updatePaginatedResults();
-		this.currentPage = 1;
-	}
-
-	private _groupBy = '';
-	get groupBy() {
-		return this._groupBy;
-	}
-	set groupBy(val: string) {
-		this._groupBy = val;
-		// this.updateResults();
-		this.updatePaginatedResults();
-	}
-
-	get badgesPluralWord(): string {
-		return this.badges.length === 1 ? this.plural['badges']['1'] : this.plural['badges']['other'];
-	}
-
-	get issuersPluralWord(): string {
-		return this.issuers.length === 1 ? this.plural['issuer']['1'] : this.plural['issuer']['other'];
-	}
-
-	isFiltered() {
-		return Boolean(this.searchQuery || this.tagsControl.value?.length);
-	}
-
-	trackById(index: number, item: any): any {
-		return item.id;
-	}
-
-	private _currentPage = 1;
-
-	get currentPage(): number {
-		return this._currentPage;
-	}
-
-	set currentPage(value: number) {
-		if (this._currentPage !== value) {
-			this._currentPage = value;
-			this.updatePaginatedResults();
-		}
-	}
-
-	groups = [this.translate.instant('Badge.category'), this.translate.instant('Badge.issuer'), '---'];
-	categoryOptions: { [key in BadgeClassCategory | 'noCategory']: string } = {
-		competency: this.translate.instant('Badge.competency'),
-		participation: this.translate.instant('Badge.participation'),
-		learningpath: this.translate.instant('Badge.learningpath'),
-		noCategory: this.translate.instant('Badge.noCategory'),
-	};
 
 	constructor(
 		protected title: Title,
@@ -177,183 +159,20 @@ export class BadgeCatalogComponent extends BaseRoutableComponent implements OnIn
 
 		// subscribe to issuer and badge class changes
 		this.badgesLoaded = this.loadBadges();
-
-		this.groupControl.valueChanges.subscribe((value) => {
-			this.groupBy = value;
-		});
-	}
-
-	async loadBadges() {
-		return new Promise(async (resolve, reject) => {
-			this.badgeClassService.allPublicBadges$.subscribe(
-				async (badges) => {
-					this.badges.set(
-						badges
-							.filter((badge) => badge.issuerVerified && badge.issuerOwnerAcceptedTos)
-							.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime()),
-					);
-
-					this.totalPages = Math.ceil(this.badges.length / this.badgesPerPage);
-					this.updatePaginatedResults();
-
-					this.badges().forEach((badge) => {
-						this.tags = this.tags.concat(badge.tags);
-						this.issuers = badge.issuerVerified ? this.issuers.concat(badge.issuer) : this.issuers;
-					});
-
-					// sortUnique sorts by frequency, we want to sort by tagname
-					// this.tags = sortUnique(this.tags);
-					this.tags = this.tags.filter((value, index, array) => array.indexOf(value) === index);
-					this.tags.sort();
-					this.tags.forEach((t) => {
-						this.tagsOptions.push({
-							label: t,
-							value: t,
-						});
-					});
-					// this.issuers = sortUnique(this.issuers);
-					this.issuers = this.issuers.filter((value, index, array) => array.indexOf(value) === index);
-					// this.updateResults();
-					resolve(badges);
-				},
-				(error) => {
-					this.messageService.reportAndThrowError('Failed to load badges', error);
-				},
-			);
-		});
-	}
-
-	async getIssuer(badge: BadgeClass): Promise<Issuer> {
-		const im = badge.issuerManager;
-		const issuer = await im.issuerBySlug(badge.issuerSlug);
-
-		return issuer;
 	}
 
 	ngOnInit() {
 		super.ngOnInit();
 
-		// initialize predefined text
-		this.prepareTexts();
-		// Translate: to update predefined text when language is changed
-		this.translate.onLangChange.subscribe((event) => {
-			this.prepareTexts();
-		});
-
 		this.tagsControl.valueChanges.subscribe(() => {
-			this.updatePaginatedResults();
-			this.currentPage = 1;
-			// this.updateResults();
+			this.currentPage.set(1);
 		});
 
 		this.sortControl.valueChanges.subscribe((value) => {
-			console.log(value);
-			this.sortOption = value;
-			this.updatePaginatedResults();
-			this.currentPage = 1;
+			this.sortOption.set(value);
+			this.currentPage.set(1);
 		});
 	}
-	prepareTexts() {
-		// 1. Groups
-		this.groups = [this.translate.instant('Badge.category'), this.translate.instant('Badge.issuer'), '---'];
-		// 2. Category options
-		this.categoryOptions = {
-			competency: this.translate.instant('Badge.competency'),
-			participation: this.translate.instant('Badge.participation'),
-			learningpath: this.translate.instant('Badge.learningpath'),
-			noCategory: this.translate.instant('Badge.noCategory'),
-		};
-		// 3. Plural
-		this.plural = {
-			issuer: {
-				'=0': this.translate.instant('Badge.noIssuers'),
-				'=1': this.translate.instant('Badge.oneIssuer'),
-				other: this.translate.instant('Badge.multiIssuers'),
-			},
-			badges: {
-				'=0': this.translate.instant('Badge.noBadges'),
-				'=1': this.translate.instant('Badge.oneBadge'),
-				other: this.translate.instant('Badge.multiBadges'),
-			},
-			recipient: {
-				'=0': this.translate.instant('Badge.noRecipients'),
-				'=1': this.translate.instant('Badge.oneRecipient'),
-				other: this.translate.instant('Badge.multiRecipients'),
-			},
-		};
-	}
-
-	private updatePaginatedResults() {
-		let that = this;
-		this.badgeResultsByIssuer = [];
-		const badgeResultsByIssuerLocal = {};
-		this.badgeResultsByCategory = [];
-		const badgeResultsByCategoryLocal = {};
-
-		const addBadgeToResultsByIssuer = function (item) {
-			let issuerResults = badgeResultsByIssuerLocal[item.issuerName];
-
-			if (!issuerResults) {
-				issuerResults = badgeResultsByIssuerLocal[item.issuerName] = new MatchingBadgeIssuer(
-					item.issuerName,
-					'',
-				);
-				that.badgeResultsByIssuer.push(issuerResults);
-			}
-
-			issuerResults.addBadge(item);
-			return true;
-		};
-
-		const addBadgeToResultsByCategory = function (item) {
-			let itemCategory =
-				item.extension && item.extension['extensions:CategoryExtension']
-					? item.extension['extensions:CategoryExtension'].Category
-					: 'noCategory';
-			let categoryResults = badgeResultsByCategoryLocal[itemCategory];
-
-			if (!categoryResults) {
-				categoryResults = badgeResultsByCategoryLocal[itemCategory] = new MatchingBadgeCategory(
-					itemCategory,
-					'',
-				);
-				that.badgeResultsByCategory.push(categoryResults);
-			}
-
-			categoryResults.addBadge(item);
-			return true;
-		};
-
-		this.filteredBadges = this.badges()
-			.filter(this.badgeMatcher(this.searchQuery))
-			.filter(
-				(badge) =>
-					!this.tagsControl.value?.length || this.tagsControl.value.some((tag) => badge.tags.includes(tag)),
-			) // Matches at least one tag
-			.filter((i) => !i.apiModel.source_url);
-
-		if (this.sortOption) {
-			applySorting(this.filteredBadges, this.sortOption);
-		}
-
-		this.totalPages = Math.ceil(this.filteredBadges.length / this.badgesPerPage);
-		const start = (this.currentPage - 1) * this.badgesPerPage;
-		const end = start + this.badgesPerPage;
-
-		that.badgeResults = this.filteredBadges.slice(start, end);
-
-		// that.badgeResults.forEach((item) => {
-		// 	addBadgeToResultsByIssuer(item);
-		// 	addBadgeToResultsByCategory(item);
-		// });
-	}
-
-	// onPageChange(newPage: number) {
-	// 	if (newPage >= 1 && newPage <= this.totalPages) {
-	// 		this.currentPage = newPage;
-	// 		this.updatePaginatedResults();
-	// 	}
-	// }
 
 	openLegend() {
 		this.showLegend.set(true);
@@ -363,16 +182,54 @@ export class BadgeCatalogComponent extends BaseRoutableComponent implements OnIn
 		this.showLegend.set(false);
 	}
 
-	filterByTag(tag) {
-		this.selectedTag = this.selectedTag == tag ? null : tag;
-		this.updatePaginatedResults();
-		// this.updateResults();
+	onSearchQueryChange(query: string) {
+		this.searchQuery.set(query);
+	}
+
+	trackById(index: number, item: any): any {
+		return item.id;
+	}
+
+	isFiltered() {
+		return Boolean(this.searchQuery || this.tagsControl.value?.length);
+	}
+
+	filterByTag(tag: ITag) {
+		this.selectedTag.update((current) => (current.value == tag.value ? null : tag));
 	}
 
 	removeTag(tag) {
 		this.tagsControl.setValue(this.tagsControl.value.filter((t) => t != tag));
-		this.updatePaginatedResults();
-		this.currentPage = 1;
+	}
+
+	private async loadBadges() {
+		return new Promise(async (resolve, reject) => {
+			this.badgeClassService.allPublicBadges$.subscribe(
+				async (badges) => {
+					this.badges.set(
+						badges
+							.filter((badge) => badge.issuerVerified && badge.issuerOwnerAcceptedTos)
+							.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime()),
+					);
+					resolve(badges);
+				},
+				(error) => {
+					this.messageService.reportAndThrowError('Failed to load badges', error);
+				},
+			);
+		});
+	}
+
+	private filterBadges(badges: BadgeClass[], query: string, sortOption) {
+		const filtered = badges
+			.filter(this.badgeMatcher(query))
+			.filter(
+				(b) => !this.tagsControl.value?.length || this.tagsControl.value.some((tag) => b.tags.includes(tag)),
+			)
+			.filter((b) => !b.apiModel.source_url);
+
+		if (sortOption !== null) applySorting(filtered, sortOption);
+		return filtered;
 	}
 
 	private badgeMatcher(inputPattern: string): (badge) => boolean {
@@ -383,58 +240,7 @@ export class BadgeCatalogComponent extends BaseRoutableComponent implements OnIn
 	}
 }
 
-class MatchingBadgeIssuer {
-	constructor(
-		public issuerName: string,
-		public badge,
-		public badges: BadgeClass[] = [],
-	) {}
-
-	async addBadge(badge) {
-		if (badge.issuerName === this.issuerName) {
-			if (this.badges.indexOf(badge) < 0) {
-				this.badges.push(badge);
-			}
-		}
-	}
-}
-
-export function sortUnique(array: string[]): string[] {
-	let frequency = {};
-
-	array.forEach(function (value) {
-		frequency[value] = 0;
-	});
-
-	let uniques = array.filter(function (value) {
-		return ++frequency[value] == 1;
-	});
-
-	return uniques.sort(function (a, b) {
-		return frequency[b] - frequency[a];
-	});
-}
-
-class MatchingBadgeCategory {
-	constructor(
-		public category: string,
-		public badge,
-		public badges: BadgeClass[] = [],
-	) {}
-
-	async addBadge(badge) {
-		if (
-			badge.extension &&
-			badge.extension['extensions:CategoryExtension'] &&
-			badge.extension['extensions:CategoryExtension'].Category === this.category
-		) {
-			if (this.badges.indexOf(badge) < 0) {
-				this.badges.push(badge);
-			}
-		} else if (!badge.extension['extensions:CategoryExtension'] && this.category == 'noCategory') {
-			if (this.badges.indexOf(badge) < 0) {
-				this.badges.push(badge);
-			}
-		}
-	}
+interface ITag {
+	label: string;
+	value: string;
 }
