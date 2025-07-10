ca6e266eb5e61f979650c2f18a69b738e451b0a5
jona159 <raabejonas@yahoo.de>
2025-07-10 15:16:10 +0200

    fix: cards on detail page

--- src/app/recipient/components/recipient-badge-collection-detail/recipient-badge-collection-detail.component.ts
+++ src/app/recipient/components/recipient-badge-collection-detail/recipient-badge-collection-detail.component.ts
@@ -166,6 +166,14 @@ export class RecipientBadgeCollectionDetailComponent extends BaseAuthenticatedRo
 	}
 
 	badgeIssueDate(entry: RecipientBadgeCollectionEntry) {
+		const context = entry.badge.apiModel.json['@context'];
+		const badgeVersion =
+			Array.isArray(context) && context.some((c) => c.indexOf('purl.imsglobal.org/spec/ob/v3p0') != -1)
+				? '3.0'
+				: '2.0';
+		if (badgeVersion == '3.0') {
+			return new Date(entry.badge.apiModel.json.validFrom);
+		}
 		return new Date(entry.badge.apiModel.json.issuedOn);
 	}
 
