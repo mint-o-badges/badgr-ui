4d7adfe4f8e329e3d53298ac623eaafc99f14211
David Scheidt <david.scheidt@opensenselab.org>
2025-07-29 16:54:01 +0200

    feat: add skill visualisation component to web components

---
+++ src/app/recipient/components/recipient-skill-visualisation/recipient-skill-visualisation.web-component.ts
@@ -0,0 +1,4 @@
+import { createWebcomponent } from 'webcomponents/create-webcomponent';
+import { RecipientSkillVisualisationComponent } from './recipient-skill-visualisation.component';
+
+createWebcomponent(RecipientSkillVisualisationComponent, 'oeb-skill-visualisation', { providers: [] });
