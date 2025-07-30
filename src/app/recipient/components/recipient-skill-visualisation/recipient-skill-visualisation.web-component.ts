6874bf9bec7a78cd2fc1c7cfb672be1b5a405bd5
David Scheidt <david.scheidt@opensenselab.org>
2025-07-30 09:15:03 +0200

    feat: draft first version of skill visualisation in web component

--- src/app/recipient/components/recipient-skill-visualisation/recipient-skill-visualisation.web-component.ts
+++ src/app/recipient/components/recipient-skill-visualisation/recipient-skill-visualisation.web-component.ts
@@ -1,4 +1,21 @@
 import { createWebcomponent } from 'webcomponents/create-webcomponent';
 import { RecipientSkillVisualisationComponent } from './recipient-skill-visualisation.component';
+import { provideHttpClient, HttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-createWebcomponent(RecipientSkillVisualisationComponent, 'oeb-skill-visualisation', { providers: [] });
+createWebcomponent(RecipientSkillVisualisationComponent, 'oeb-skill-visualisation', {
+	providers: [
+		provideHttpClient(),
+		importProvidersFrom(
+			TranslateModule.forRoot({
+				loader: {
+					provide: TranslateLoader,
+					useFactory: (client) => new TranslateHttpLoader(client),
+					deps: [HttpClient],
+				},
+			}),
+		),
+	],
+});
