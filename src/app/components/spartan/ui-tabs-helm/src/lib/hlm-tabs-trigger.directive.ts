b8fdd4ac380e751ab2bf153da4beed87dc26e155
jona159 <raabejonas@yahoo.de>
2025-08-12 15:55:15 +0200

    feat: add variant for tabs trigger

--- src/app/components/spartan/ui-tabs-helm/src/lib/hlm-tabs-trigger.directive.ts
+++ src/app/components/spartan/ui-tabs-helm/src/lib/hlm-tabs-trigger.directive.ts
@@ -7,8 +7,10 @@ import type { ClassValue } from 'clsx';
 export const tabsVariants = cva('', {
 	variants: {
 		variant: {
-			default: 'data-[state=inactive]:tw-border-white',
+			default:
+				'data-[state=inactive]:tw-border-white data-[state=active]:tw-text-purple data-[state=active]:tw-border-purple',
 			lightpurple: 'data-[state=inactive]:tw-border-lightpurple',
+			black: 'data-[state=inactive]:tw-border-[#CCD7FF] data-[state=active]:tw-border-oebblack',
 		},
 	},
 	defaultVariants: {
@@ -39,7 +41,7 @@ export class HlmTabsTriggerDirective {
 			tabsVariants({
 				variant: this._variant(),
 			}),
-			'tw-text-oebblack tw-text-[14px] tw-leading-[19px] md:tw-text-[20px] md:tw-leading-[28px] tw-inline-flex tw-items-center tw-justify-center tw-whitespace-nowrap tw-px-3 tw-py-1.5 tw-transition-all focus-visible:tw-outline-none focus-visible:tw-ring-2 focus-visible:tw-ring-ring focus-visible:tw-ring-offset-2 disabled:tw-pointer-events-none disabled:tw-opacity-50 data-[state=active]:tw-text-purple tw-border-solid tw-border-b-2 tw-border-white data-[state=active]:tw-font-medium data-[state=active]:tw-border-purple data-[state=active]:tw-shadow-sm',
+			'tw-text-oebblack tw-text-[14px] tw-leading-[19px] md:tw-text-[20px] md:tw-leading-[28px] tw-inline-flex tw-items-center tw-justify-center tw-whitespace-nowrap tw-px-3 tw-py-1.5 tw-transition-all focus-visible:tw-outline-none focus-visible:tw-ring-2 focus-visible:tw-ring-ring focus-visible:tw-ring-offset-2 disabled:tw-pointer-events-none disabled:tw-opacity-50 h tw-border-solid tw-border-b-2 tw-border-white data-[state=active]:tw-font-medium data-[state=active]:tw-shadow-sm',
 			this.userClass(),
 			this._settableClass(),
 		),
