3d55cf63ff4fb968e6d709b133c5f6c27f9df541
David Scheidt <david.scheidt@opensenselab.org>
2025-09-12 16:53:19 +0200

    fix: increase padding of text for oeb-checkbox

--- src/app/components/oeb-checkbox.component.ts
+++ src/app/components/oeb-checkbox.component.ts
@@ -34,7 +34,7 @@ import { HlmP } from '@spartan-ng/helm/typography';
 			[disabled]="disabled"
 		/>
 		<div class="tw-flex tw-flex-col">
-			<span class="tw-pl-[3px]" [innerHTML]="text"></span>
+			<span class="tw-pl-[12px]" [innerHTML]="text"></span>
 			@if (isErrorState) {
 				<oeb-input-error class="tw-text-red tw-pl-[3px]" [error]="errorMessageForDisplay"></oeb-input-error>
 			}
