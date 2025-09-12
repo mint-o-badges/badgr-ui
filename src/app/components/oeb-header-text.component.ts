d8b216c9468d8f275224c25961f82f8a4ef51ecc
David Scheidt <david.scheidt@opensenselab.org>
2025-09-12 16:20:48 +0200

    feat: add templating capabilities to header text component

--- src/app/components/oeb-header-text.component.ts
+++ src/app/components/oeb-header-text.component.ts
@@ -1,25 +1,42 @@
-import { Component, input } from '@angular/core';
+import { NgTemplateOutlet } from '@angular/common';
+import { Component, input, TemplateRef, viewChild } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { HlmH1 } from '@spartan-ng/helm/typography';
 
+/**
+ * Standard header component to show double-line header text
+ * including styles and offset effect.
+ * Supports setting texts via templates, if effects or similar
+ * are applied to it.
+ * Texts passed to the component are translated by default.
+ */
 @Component({
 	selector: 'oeb-header-text',
-	imports: [TranslatePipe, HlmH1],
+	imports: [TranslatePipe, HlmH1, NgTemplateOutlet],
 	template: `
 		<h1 hlmH1>
 			<span class="tw-font-normal tw-text-oebblack">
-				@if (translate()) {
-					{{ text1() | translate }}
+				@if (text1Template()) {
+					<ng-container *ngTemplateOutlet="text1Template()" />
 				} @else {
-					{{ text1() }}
+					@if (translate()) {
+						{{ text1() | translate }}
+					} @else {
+						{{ text1() }}
+					}
 				}
 			</span>
+
 			<br />
 			<span class="tw-font-black tw-text-purple md:tw-pl-[2ch]">
-				@if (translate()) {
-					{{ text2() | translate }}
+				@if (text2Template()) {
+					<ng-container *ngTemplateOutlet="text2Template()" />
 				} @else {
-					{{ text2() }}
+					@if (translate()) {
+						{{ text2() | translate }}
+					} @else {
+						{{ text2() }}
+					}
 				}
 			</span>
 		</h1>
@@ -28,19 +45,36 @@ import { HlmH1 } from '@spartan-ng/helm/typography';
 export class OebHeaderText {
 	/**
 	 * Controls whether the component should translate the
-	 * texts passed into {@link text1} and {@link text2}
+	 * texts passed into {@link text1} and {@link text2}.
+	 * This option is ignored when templates are passed.
 	 */
 	translate = input<boolean>(true);
 
 	/**
 	 * Upper text of standardized double-line header text.
 	 * Displayed in oebblack color and with normal font weight.
+	 * Has no effect when {@link text1Template} is used.
 	 */
-	text1 = input.required<string>();
+	text1 = input<string>();
+
+	/**
+	 * Template to be used for upper text of standardized double-line header text.
+	 * Styles are applied to the template, but not translations are applied.
+	 * Overrides any usage of {@link text1}.
+	 */
+	text1Template = input<TemplateRef<any>>();
 
 	/**
 	 * Bottom text of standardized double-line header text.
 	 * Displayed in oeb purple color and with bold font weight.
+	 * Has no effect when {@link text2Template} is used.
 	 */
-	text2 = input.required<string>();
+	text2 = input<string>();
+
+	/**
+	 * Template to be used for upper text of standardized double-line header text.
+	 * Styles are applied to the template, but not translations are applied.
+	 * Overrides any usage of {@link text2}.
+	 */
+	text2Template = input<TemplateRef<any>>();
 }
