650ed32d7c98a9579dbd1e50702d08a2a3ef5376
David Scheidt <david.scheidt@opensenselab.org>
2025-09-29 16:57:57 +0200

    build: upgrade eslint and use flat config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,41 +1,39 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all,
 });
 
-export default defineConfig([globalIgnores(["projects/**/*"]), {
-    files: ["**/*.ts"],
-
-    extends: compat.extends(
-        "plugin:@angular-eslint/recommended",
-        "plugin:@angular-eslint/template/process-inline-templates",
-        "plugin:prettier/recommended",
-    ),
-
-    languageOptions: {
-        ecmaVersion: 5,
-        sourceType: "script",
-
-        parserOptions: {
-            project: ["tsconfig.json"],
-            createDefaultProgram: true,
-        },
-    },
-
-    rules: {
-        "@angular-eslint/prefer-inject": "off",
-    },
-}, {
-    files: ["**/*.html"],
-    extends: compat.extends("plugin:@angular-eslint/template/recommended"),
-    rules: {},
-}]);
+export default [
+	...compat.extends(
+		'plugin:@angular-eslint/recommended',
+		'plugin:@angular-eslint/template/process-inline-templates',
+		'plugin:prettier/recommended',
+	),
+	{
+		files: ['**/*.ts'],
+		languageOptions: {
+			ecmaVersion: 5,
+			sourceType: 'script',
+			parserOptions: {
+				project: ['tsconfig.json'],
+				createDefaultProgram: true,
+			},
+		},
+		rules: {
+			'@angular-eslint/prefer-inject': 'off',
+		},
+	},
+	...compat.extends('plugin:@angular-eslint/template/recommended'),
+	{
+		files: ['**/*.html'],
+		rules: {},
+	},
+];
