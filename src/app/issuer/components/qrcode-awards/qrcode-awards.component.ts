07e50b815c1f5bda0627a221e0cd3739d0695b07
jona159 <raabejonas@yahoo.de>
2025-10-27 11:55:54 +0100

    feat: add dates to qrcode datatable batch award

--- src/app/issuer/components/qrcode-awards/qrcode-awards.component.ts
+++ src/app/issuer/components/qrcode-awards/qrcode-awards.component.ts
@@ -23,6 +23,7 @@ import { SvgIconComponent } from '~/common/components/svg-icon.component';
 import { HlmIcon } from '@spartan-ng/helm/icon';
 import { HlmH3 } from '@spartan-ng/helm/typography';
 import { Network } from '~/issuer/network.model';
+import { ApiQRCode } from '~/issuer/models/qrcode-api.model';
 
 @Component({
 	selector: 'qrcode-awards',
@@ -63,15 +64,13 @@ export class QrCodeAwardsComponent implements OnChanges {
 		}
 	}
 
-	@Input() awards: any[];
+	@Input() awards: ApiQRCode[];
 	@Input() routerLinkText: string[];
 	@Input() issuer: Issuer | Network;
 	@Input() badgeClass: BadgeClass;
 	@Input() defaultUnfolded: boolean | undefined = false;
 	@Input() interactive = true;
 	@Output() qrBadgeAward = new EventEmitter<number>();
-
-	requestedBadges: any[] = [];
 
 	qrCodeMenus: Array<MenuItem[]> = [];
 
