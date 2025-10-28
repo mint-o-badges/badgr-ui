7df275b9ba7d7460f78524fb22888130b321b271
David Scheidt <david.scheidt@opensenselab.org>
2025-10-28 14:05:07 +0100

    fix: forgot one

--- src/app/recipient/recipient.routes.ts
+++ src/app/recipient/recipient.routes.ts
@@ -1,12 +1,5 @@
 import { Routes } from '@angular/router';
 
-
-
-
-
-
-
-
 export const routes: Routes = [
 	/* Recipient Badges */
 	{
@@ -16,33 +9,54 @@ export const routes: Routes = [
 	},
 	{
 		path: 'badges',
-		loadComponent: () => import('./components/recipient-earned-badge-list/recipient-earned-badge-list.component').then(m => m.RecipientEarnedBadgeListComponent),
+		loadComponent: () =>
+			import('./components/recipient-earned-badge-list/recipient-earned-badge-list.component').then(
+				(m) => m.RecipientEarnedBadgeListComponent,
+			),
 	},
 	{
 		path: 'badges/import',
-		loadComponent: () => import('./components/recipient-earned-badge-list/recipient-earned-badge-list.component').then(m => m.RecipientEarnedBadgeListComponent),
+		loadComponent: () =>
+			import('./components/recipient-earned-badge-list/recipient-earned-badge-list.component').then(
+				(m) => m.RecipientEarnedBadgeListComponent,
+			),
 	},
 	{
 		path: 'earned-badge/:badgeSlug',
-		loadComponent: () => import('./components/recipient-earned-badge-detail/recipient-earned-badge-detail.component').then(m => m.RecipientEarnedBadgeDetailComponent),
+		loadComponent: () =>
+			import('./components/recipient-earned-badge-detail/recipient-earned-badge-detail.component').then(
+				(m) => m.RecipientEarnedBadgeDetailComponent,
+			),
 	},
 	{
 		path: 'imported-badge/:badgeSlug',
-		loadComponent: () => import('./components/imported-badge-detail/imported-badge-detail.component').then(m => m.ImportedBadgeDetailComponent),
+		loadComponent: () =>
+			import('./components/imported-badge-detail/imported-badge-detail.component').then(
+				(m) => m.ImportedBadgeDetailComponent,
+			),
 	},
 
 	/* Recipient Badge Collections */
 	{
 		path: 'badge-collections/create',
-		loadComponent: () => import('./components/recipient-badge-collection-create/recipient-badge-collection-create.component').then(m => m.RecipientBadgeCollectionCreateComponent),
+		loadComponent: () =>
+			import('./components/recipient-badge-collection-create/recipient-badge-collection-create.component').then(
+				(m) => m.RecipientBadgeCollectionCreateComponent,
+			),
 	},
 	{
 		path: 'badge-collections/:collectionSlug/edit',
-		loadComponent: () => import('~/recipient-badge-collection-edit/recipient-badge-collection-edit.component').then(m => m.RecipientBadgeCollectionEditComponent),
+		loadComponent: () =>
+			import('~/recipient-badge-collection-edit/recipient-badge-collection-edit.component').then(
+				(m) => m.RecipientBadgeCollectionEditComponent,
+			),
 	},
 	{
 		path: 'badge-collections/collection/:collectionSlug',
-		loadComponent: () => import('./components/recipient-badge-collection-detail/recipient-badge-collection-detail.component').then(m => m.RecipientBadgeCollectionDetailComponent),
+		loadComponent: () =>
+			import('./components/recipient-badge-collection-detail/recipient-badge-collection-detail.component').then(
+				(m) => m.RecipientBadgeCollectionDetailComponent,
+			),
 	},
 	{
 		path: '**',
