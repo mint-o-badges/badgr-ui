5275d0f337ac8696b27cb7bcb1cceb493ff86ea3
jona159 <raabejonas@yahoo.de>
2025-10-31 10:57:57 +0100

    feat: reintroduce grouping

--- src/app/issuer/components/learningpath-edit-form/learningpath-edit-form.component.ts
+++ src/app/issuer/components/learningpath-edit-form/learningpath-edit-form.component.ts
@@ -206,6 +206,9 @@ export class LearningPathEditFormComponent
 	badgeResults: BadgeResult[] = null;
 	badgesFormArray: any;
 
+	badgeResultsByCategory: MatchingBadgeCategory[] = [];
+	badgeResultsByIssuer: MatchingBadgeIssuer[] = [];
+
 	selectedTag: string = null;
 	order = 'asc';
 	private _searchQuery = '';
@@ -216,6 +219,23 @@ export class LearningPathEditFormComponent
 		this._searchQuery = query;
 		this.updateResults();
 	}
+
+	private _groupBy = '---';
+	get groupBy() {
+		return this._groupBy;
+	}
+	set groupBy(val: string) {
+		this._groupBy = val;
+		this.updateResults();
+	}
+
+	groups: string[] = [];
+	categoryOptions: { [key in BadgeClassCategory | 'noCategory']: string } = {
+		competency: '',
+		participation: '',
+		learningpath: '',
+		noCategory: '',
+	};
 
 	get imageFieldDirty() {
 		return (
@@ -234,6 +254,9 @@ export class LearningPathEditFormComponent
 		this.baseUrl = this.configService.apiConfig.baseUrl;
 		this.issuerLoaded = this.issuerManager.issuerBySlug(this.issuerSlug).then((issuer) => {
 			this.issuer = issuer;
+			if (issuer.is_network) {
+				this.learningPathForm.rawControl.controls.useIssuerImageInBadge.setValue(false);
+			}
 			this.badgesLoaded = this.loadBadges();
 		});
 	}
@@ -270,14 +293,12 @@ export class LearningPathEditFormComponent
 	}
 
 	ngOnInit() {
+		this.groups[0] = this.translate.instant('Badge.category');
+		this.groups[1] = this.translate.instant('General.institution');
 		this.fetchTags();
 		if (!this.initialisedLearningpath) {
 			this.learningPathForm.controls.license.addFromTemplate();
 		}
-
-		// if (this.issuer.is_network) {
-		// 	this.learningPathForm.rawControl.controls.useIssuerImageInBadge.setValue(false);
-		// }
 
 		this.draggableList = this.selectedBadges.map((badge, index) => {
 			return {
@@ -600,20 +621,70 @@ export class LearningPathEditFormComponent
 		this.order = order;
 		if (this.order === 'asc') {
 			this.badgeResults.sort((a, b) => a.name.localeCompare(b.name));
+			this.badgeResultsByIssuer
+				.sort((a, b) => a.issuerName.localeCompare(b.issuerName))
+				.forEach((r) => r.badges.sort((a, b) => a.name.localeCompare(b.name)));
+			this.badgeResultsByCategory
+				.sort((a, b) => a.category.localeCompare(b.category))
+				.forEach((r) => r.badges.sort((a, b) => a.name.localeCompare(b.name)));
 		} else {
 			this.badgeResults.sort((a, b) => b.name.localeCompare(a.name));
+			this.badgeResultsByIssuer
+				.sort((a, b) => b.issuerName.localeCompare(a.issuerName))
+				.forEach((r) => r.badges.sort((a, b) => b.name.localeCompare(a.name)));
+			this.badgeResultsByCategory
+				.sort((a, b) => b.category.localeCompare(a.category))
+				.forEach((r) => r.badges.sort((a, b) => b.name.localeCompare(a.name)));
 		}
 	}
 
 	private updateResults() {
+		// eslint-disable-next-line @typescript-eslint/no-this-alias
+		let that = this;
 		// Clear Results
 		this.badgeResults = [];
+		this.badgeResultsByIssuer = [];
+		const badgeResultsByIssuerLocal = {};
+		this.badgeResultsByCategory = [];
+		const badgeResultsByCategoryLocal = {};
+		var addBadgeToResultsByIssuer = function (item) {
+			let issuerResults = badgeResultsByIssuerLocal[item.issuerName];
+			if (!issuerResults) {
+				issuerResults = badgeResultsByIssuerLocal[item.issuerName] = new MatchingBadgeIssuer(
+					item.issuerName,
+					'',
+				);
+				// append result to the issuerResults array bound to the view template.
+				that.badgeResultsByIssuer.push(issuerResults);
+			}
+			issuerResults.addBadge(item);
+			return true;
+		};
+		var addBadgeToResultsByCategory = (item) => {
+			let itemCategory =
+				item.extension && item.extension['extensions:CategoryExtension']
+					? item.extension['extensions:CategoryExtension'].Category
+					: 'noCategory';
+			let categoryResults = badgeResultsByCategoryLocal[itemCategory];
+			if (!categoryResults) {
+				categoryResults = badgeResultsByCategoryLocal[itemCategory] = new MatchingBadgeCategory(
+					itemCategory,
+					'',
+				);
+				// append result to the categoryResults array bound to the view template.
+				this.badgeResultsByCategory.push(categoryResults);
+			}
+			categoryResults.addBadge(item);
+			return true;
+		};
 		this.badges
 			.filter(this.badgeMatcher(this.searchQuery))
 			.filter(this.badgeTagMatcher(this.selectedTag))
 			.filter((i) => !i.apiModel.source_url)
 			.forEach((item) => {
 				this.badgeResults.push(item);
+				addBadgeToResultsByIssuer(item);
+				addBadgeToResultsByCategory(item);
 			});
 	}
 
@@ -900,5 +971,43 @@ export class LearningPathEditFormComponent
 		return this.selectedBadges.length >= 2
 			? null
 			: { minSelectedBadges: { required: 2, actual: this.selectedBadges.length } };
+	}
+}
+
+class MatchingBadgeCategory {
+	constructor(
+		public category: string,
+		public badge,
+		public badges: BadgeClass[] = [],
+	) {}
+	async addBadge(badge) {
+		if (
+			badge.extension &&
+			badge.extension['extensions:CategoryExtension'] &&
+			badge.extension['extensions:CategoryExtension'].Category === this.category
+		) {
+			if (this.badges.indexOf(badge) < 0) {
+				this.badges.push(badge);
+			}
+		} else if (!badge.extension['extensions:CategoryExtension'] && this.category == 'noCategory') {
+			if (this.badges.indexOf(badge) < 0) {
+				this.badges.push(badge);
+			}
+		}
+	}
+}
+
+class MatchingBadgeIssuer {
+	constructor(
+		public issuerName: string,
+		public badge,
+		public badges: BadgeClass[] = [],
+	) {}
+	async addBadge(badge) {
+		if (badge.issuerName === this.issuerName) {
+			if (this.badges.indexOf(badge) < 0) {
+				this.badges.push(badge);
+			}
+		}
 	}
 }
