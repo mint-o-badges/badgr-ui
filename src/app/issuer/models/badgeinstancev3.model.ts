33ec2ffcd12e4bfafd0473147e82b8d1ac3de135
jona159 <raabejonas@yahoo.de>
2025-11-21 07:16:30 +0100

    feat: v3 badgeinstance models

---
+++ src/app/issuer/models/badgeinstancev3.model.ts
@@ -0,0 +1,28 @@
+import { ApiBadgeInstance } from './badgeinstance-api.model';
+
+export class BadgeInstanceV3 {
+	slug: string;
+	created_at: string;
+	issued_on: string;
+	expires: string | null;
+	recipient_identifier: string;
+	recipient_type: string;
+	revoked: boolean;
+	revocation_reason: string | null;
+	extensions: any;
+	public_url: string;
+	badge_class: any;
+	issuer: any;
+
+	constructor(data: ApiBadgeInstance) {
+		Object.assign(this, data);
+	}
+
+	get issuedOn(): Date {
+		return new Date(this.issued_on);
+	}
+
+	getExtension(name: string, defaultValue: any = null) {
+		return this.extensions && this.extensions[name] ? this.extensions[name] : defaultValue;
+	}
+}
